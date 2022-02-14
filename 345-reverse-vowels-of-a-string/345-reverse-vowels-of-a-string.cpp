374b0cb3ad217bfd32e252030810d92a517f26b3
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-02-14 20:00:01 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (34.71%) - LeetHub

---
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,20 @@
+// #define push_back pb
+class Solution {
+public:
+    string reverseVowels(string s) {
+        vector<char>vec;
+        for(auto ch:s){
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'|| ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+                vec.push_back(ch);
+            }
+        }
+        reverse(vec.begin(),vec.end());
+        int i=0;
+        for(auto&ch:s){
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'|| ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+                ch=vec[i++];
+            }
+        }
+        return s;
+    }
+};
