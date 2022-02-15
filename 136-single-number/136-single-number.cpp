1ed7163a751b60cc187cf770712b3384c34950d0
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-02-15 10:12:05 +0530

    Time: 38 ms (26.69%), Space: 19.9 MB (16.80%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
+        unordered_set<int>st;
         for(auto ele:nums){
-            ans=ans^ele;
+            if(st.find(ele)!=st.end()){
+                st.erase(ele);
+            }
+            else{
+                st.insert(ele);
+            }
         }
-        return ans;
+        return *st.begin();
     }
 };
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int ans=0;
+//         for(auto ele:nums){
+//             ans=ans^ele;
+//         }
+//         return ans;
+//     }
+// };
