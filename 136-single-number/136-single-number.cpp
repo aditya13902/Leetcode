65131ea80a5eb675c35ed1325ad4587bc5540b08
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-02-15 10:16:43 +0530

    Time: 26 ms (55.88%), Space: 20.1 MB (14.62%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int>st;
+        unordered_map<int,int>mp;
         for(auto ele:nums){
-            if(st.find(ele)!=st.end()){
-                st.erase(ele);
-            }
-            else{
-                st.insert(ele);
+            mp[ele]++;
+        }
+        for(auto itr:mp){
+            if(itr.second==1){
+                return itr.first;
             }
         }
-        return *st.begin();
+        return -1;
     }
 };
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         unordered_set<int>st;
+//         for(auto ele:nums){
+//             if(st.find(ele)!=st.end()){
+//                 st.erase(ele);
+//             }
+//             else{
+//                 st.insert(ele);
+//             }
+//         }
+//         return *st.begin();
+//     }
+// };
 // class Solution {
 // public:
 //     int singleNumber(vector<int>& nums) {
