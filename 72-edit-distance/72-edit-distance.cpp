c2cabf60504623af94a01f368f508a76953ca159
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-02-17 12:49:23 +0530

    Time: 28 ms (33.27%), Space: 9.1 MB (34.06%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -6,10 +6,10 @@ public:
         
         vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
         for(int i = 1; i <= n; i++){
-            dp[i][0] = 1 + dp[i - 1][0];
+            dp[i][0] = i;
         }
         for(int i = 1; i <= m; i++){
-            dp[0][i] = 1 + dp[0][i - 1];
+            dp[0][i] = i;
         }
         
         for(int i = 1; i <= n; i++){
