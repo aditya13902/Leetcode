6a0baaddcd17249acfc521ba54f45605edcd5792
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-02-20 09:52:55 +0530

    Time: 51 ms (25.44%), Space: 11.4 MB (59.43%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -9,18 +9,41 @@ public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end(),cmp);
         int cnt=0;
+        int mxend=intervals[0][1];
         for(int i=1;i<intervals.size();i++){
-            int s=intervals[i][0];
-            int e=intervals[i][1];
-            for(int j=i-1;j>=0;j--){
-                int currs=intervals[j][0];
-                int curre=intervals[j][1];
-                if(s>=currs && e<=curre){
-                    cnt++;
-                    break;
-                }
+            if(intervals[i][1]<=mxend){
+                cnt++;
+            }
+            else{
+                mxend=intervals[i][1];
             }
         }
         return intervals.size()-cnt;
     }
 };
+// class Solution {
+// public:
+//     static bool cmp(vector<int>&v1 ,vector<int>&v2){
+//         if(v1[0]==v2[0]){
+//             return v1[1]>v2[1];
+//         }
+//         return v1[0]<v2[0];
+//     }
+//     int removeCoveredIntervals(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(),intervals.end(),cmp);
+//         int cnt=0;
+//         for(int i=1;i<intervals.size();i++){
+//             int s=intervals[i][0];
+//             int e=intervals[i][1];
+//             for(int j=i-1;j>=0;j--){
+//                 int currs=intervals[j][0];
+//                 int curre=intervals[j][1];
+//                 if(s>=currs && e<=curre){
+//                     cnt++;
+//                     break;
+//                 }
+//             }
+//         }
+//         return intervals.size()-cnt;
+//     }
+// };
