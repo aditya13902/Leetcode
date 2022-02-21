39ba515251cd7b7f7fde6037d6b82d9bfa53a673
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-02-21 14:08:34 +0530

    Time: 904 ms (8.66%), Space: 56.9 MB (17.47%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int ans=0;
+    void dfs(int n,int pos,vector<bool>flag){
+        if(pos>n){
+            ans++;
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            if(!flag[i] && (i%pos==0 || pos%i ==0)){
+                flag[i]=true;
+                dfs(n,pos+1,flag);
+                flag[i]=false;  //backtrack
+            }
+        }
+    }
+    int countArrangement(int n) {
+        vector<bool>flag(16,false);
+        if(n<4){
+            return n;
+        }
+        dfs(n,1,flag);
+        return ans;
+        
+    }
+};
