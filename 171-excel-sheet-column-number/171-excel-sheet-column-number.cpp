3a8bc85cc41e177e4c1a5db0446518e53985b971
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-02-22 09:25:57 +0530

    Time: 5 ms (22.87%), Space: 5.8 MB (92.06%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -4,7 +4,7 @@ public:
         int res=0;
         for(char ch:columnTitle){
             int ele=ch-'A'+1;
-            cout<<ch-'A'+1<<endl;
+            // cout<<ch-'A'+1<<endl;
             res=res*26+ele;
         }
         return res;
