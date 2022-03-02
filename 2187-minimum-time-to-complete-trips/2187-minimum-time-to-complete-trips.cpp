c46b32d406b5236028f2da8660a797bc4695df14
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-03-02 19:26:58 +0530

    Time: 356 ms (73.66%), Space: 94.5 MB (5.52%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool helper(vector<int>&time,int totalTrips,long long t){
-        for(auto ele:time){
-            if(totalTrips<=0){
-                break;
-            }
-            totalTrips-=(t/ele);
+	// this function will count totalTrips for the given time
+	// a = [1,2,3] , and at time 3 how many trips we can take? 
+	// 3/1 + 3/2 + 3/3 => 3 + 1 + 1 = 5 Trips
+    long long int numberOfTripsForGivenTime(vector<int>&a , long long int givenTime)
+    {
+        long long int totalTrips = 0;
+        for(auto x : a)
+        {
+            // convert it to long long int 
+            long long int val = x;
+			
+            totalTrips += (givenTime / val);
         }
-        return totalTrips<=0;
+        return totalTrips;
     }
-    long long minimumTime(vector<int>& time, int totalTrips) {
-        // sort(time.begin(),time.end());
-        long long mx=0;
-        for(auto ele:time){
-            if(mx<ele){
-                mx=ele;
-            }
+    long long minimumTime(vector<int>& arr ,  int totalTrips) {
+        long long int lowestTime = 1;
+        long long int highestTime = 1e14;
+        while(lowestTime<highestTime)
+        {
+            long long int mid = lowestTime + (highestTime-lowestTime)/2;
+            
+            if(numberOfTripsForGivenTime(arr , mid) >= totalTrips)
+                highestTime = mid;
+            else
+                lowestTime = mid+1;
         }
-        long long s=1;
-        long long e=mx*totalTrips+1;
-        while(s<e){
-            long long mid=s+(e-s)/2;
-            if(helper(time,totalTrips,mid)){
-                e=mid;
-            }
-            else{
-                s=mid+1;
-            }
-        }
-        return s;
+        return lowestTime;
     }
 };
