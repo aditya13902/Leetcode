e8a901c1d035a428e22173cf680b31bba280e148
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-03-18 09:28:53 +0530

    Time: 11 ms (10.73%), Space: 7 MB (13.91%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int> lastIndex(26, 0);
-        for (int i = 0; i < s.length(); i++){
-            lastIndex[s[i] - 'a'] = i; // track the lastIndex of character presence
+        unordered_map<char,int>mp1;
+        unordered_map<char,bool>mp2;
+        for(int i=0;i<s.size();i++){
+            mp1[s[i]]=i;
+            mp2[s[i]]=false;
         }
         
-        vector<bool> seen(26, false); // keep track seen
-        stack<char> st;
         
-        for (int i = 0; i < s.size(); i++) {
-            int curr = s[i] - 'a';
-            if (seen[curr]) continue; // if seen continue as we need to pick one char only
-            while(st.size() > 0 && st.top() > s[i] && i < lastIndex[st.top() - 'a']){
-                seen[st.top() - 'a'] = false; // pop out and mark unseen
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(mp2[s[i]]){
+                continue;
+            }
+            while(st.size()>0 && st.top()>s[i] && mp1[st.top()]>i){
+                mp2[st.top()]=false;
                 st.pop();
             }
-            st.push(s[i]); // add into stack
-            seen[curr] = true; // mark seen
+            st.push(s[i]);
+            mp2[s[i]]=true;
         }
-        
         string ans = "";
         while (st.size() > 0){
             ans += st.top();
             st.pop();
         }
         reverse(ans.begin(), ans.end());
+        
         return ans;
     }
 };
