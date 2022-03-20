da479c1880ebe1c207f91e5e8eff6df2a8e0a027
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-03-20 18:48:51 +0530

    Time: 501 ms (5.01%), Space: 70.4 MB (10.40%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    unordered_map<int,int>from;
-    unordered_map<int,int>to;
     int findJudge(int n, vector<vector<int>>& trust) {
-        for(auto vec:trust){
-            from[vec[0]]++;
-            to[vec[1]]++;
+        unordered_map<int,int>from;
+        unordered_map<int,int>to;
+        for(auto ele:trust){
+            from[ele[0]]++;
+            to[ele[1]]++;
         }
         for(int i=1;i<=n;i++){
-            if(from[i]==0 && to[i]==n-1){
+            if(to[i]==n-1 && from[i]==0){
                 return i;
             }
         }
