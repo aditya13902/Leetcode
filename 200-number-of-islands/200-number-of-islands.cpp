6a6c838e2d94ff0a2f24e166cc05e9c3d1dc0846
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-03-20 15:32:36 +0530

    Time: 44 ms (62.92%), Space: 13.3 MB (31.21%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     vector<vector<int>>vis;
-    int cnt=0;
-    void dfs(vector<vector<char>>& grid,int i,int j){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()){
+    void dfs(vector<vector<char>>&grid,int i,int j){
+        if(i<0 || j<0 ||i>=grid.size() ||j>=grid[0].size() || grid[i][j]=='0' || vis[i][j]){
             return;
         }
-        if(grid[i][j]=='0'){
-            return;
-        }
-        if(vis[i][j]==1){
-            return;
-        }
+        // subans++;
         vis[i][j]=1;
         dfs(grid,i+1,j);
         dfs(grid,i,j+1);
@@ -19,18 +13,18 @@ public:
         dfs(grid,i,j-1);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vis.resize(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        vis.resize(grid.size(),vector<int>(grid[0].size(),0));
+        int ans=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1' && vis[i][j]==0){
-                    cnt++;
+                    // int subans=0;
                     dfs(grid,i,j);
+                    ans++;
                 }
             }
         }
-        return cnt;
+        return ans;
         
     }
 };
