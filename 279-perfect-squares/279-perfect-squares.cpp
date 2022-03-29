382a68e3678653dca2323a7a1233e1e6c133d7a8
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-03-29 22:27:04 +0530

    Time: 257 ms (43.25%), Space: 9.1 MB (61.71%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>dp(n+1);
         dp[1]=1;
         dp[0]=0;
-        for(int i=1;i<=n;i++){
+        for(int i=2;i<=n;i++){
             dp[i]=i;
             for(int j=1;j*j<=i;j++){
                 dp[i]=min(dp[i],dp[i-j*j]+1);
