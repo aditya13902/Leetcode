e5ad936e4d48931ebc90f85de6f34c9d00a479e5
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-04-17 14:00:35 +0530

    Time: 4 ms (70.88%), Space: 8.8 MB (89.82%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -4,12 +4,12 @@ public:
         int l=0;
         int r=nums.size()-1;
         while(l<r){
-            int m=(l+r+1)/2;
-            if(nums[m]<nums[m-1]){
-                r=m-1;
+            int m=(l+r)/2;
+            if(nums[m]<nums[m+1]){
+                l=m+1;
             }
             else{
-                l=m;
+                r=m;
             }
         }
         return l;
