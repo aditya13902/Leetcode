05c4d23835f0022188870a4b060d2bfa8c2c2342
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-11 17:32:40 +0530

    Time: 363 ms (39.40%), Space: 6.5 MB (94.38%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    bool check(string& s,int i,int j){
-        while(i<j){
-            if(s[i]!=s[j]){
+    bool check(string& str,int s,int e){
+        while(s<e){
+            if(str[s]!=str[e]){
                 return false;
             }
-            i++;
-            j--;
+            s++;
+            e--;
         }
         return true;
     }
     int minCut(string s) {
         int n=s.size();
-        vector<int>dp(n+1,INT_MAX);
-        dp[n-1]=0;
-        // dp[n]=0;
-        for(int i=n-2;i>=0;i--){
-            for(int j=i;j<n;j++){
-                if(check(s,i,j)){
-                    if(j==n-1){
+        vector<int>dp(n);
+        dp[0]=0;
+        for(int i=1;i<n;i++){
+            dp[i]=i;
+            for(int j=0;j<=i;j++){
+                if(check(s,j,i)){
+                    if(j==0){
                         dp[i]=0;
                         break;
                     }
-                    int subans=1+dp[j+1];
-                    dp[i]=min(dp[i],subans);
+                    dp[i]=min(dp[i],1+dp[j-1]);
                 }
             }
         }
-        return dp[0];
+        return dp[n-1];
     }
 };
