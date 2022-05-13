afd36689af737e94ecbe5d834baa43566561c825
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-13 17:58:25 +0530

    Time: 181 ms (38.35%), Space: 93.3 MB (53.78%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int mn=prices[0];
         int ans=0;
         for(int i=1;i<prices.size();i++){
-            ans=max(ans,prices[i]-mn);
+            ans=max(prices[i]-mn,ans);
             mn=min(prices[i],mn);
         }
         return ans;
