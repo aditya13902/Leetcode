c5fba5cf1fd86ec44f863db1a004649a08402e05
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-16 17:50:21 +0530

    Time: 131 ms (37.34%), Space: 31.4 MB (32.66%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -6,22 +6,19 @@ public:
         int dx[8]={0,1,1,-1,0,-1,1,-1};
         int dy[8]={1,0,1,0,-1,-1,-1,1};
         int cnt=0;
-        map<pair<int,int>,int>mp;
+        // map<pair<int,int>,int>mp;
         while(q.size()){
             int l=q.size();
             while(l--){
                 auto top=q.front();
                 q.pop();
-                if(mp[top]){
-                    continue;
-                }
                 if(grid[top.first][top.second]!=0){
                     continue;
                 }
-                mp[top]=1;
                 if(top.first==grid.size()-1 && top.second==grid[0].size()-1){
                     return ++cnt;
                 }
+                grid[top.first][top.second]=1;
                 for(int i=0;i<8;i++){
                     int x=top.first+dx[i];
                     int y=top.second+dy[i];
