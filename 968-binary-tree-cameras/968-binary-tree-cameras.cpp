0b38be3f5ccd779b112642e4bf857995ae27522e
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-19 22:49:23 +0530

    Time: 3 ms (99.28%), Space: 21.4 MB (27.43%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -9,49 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#define tn TreeNode* 
-#define inf (1<<20)
-
-
-map<pair<tn, pair<bool, bool>>, int> dp; 
-
 class Solution {
-    int solve(tn root, bool cam, bool parCam){
-        if(root == NULL){
-            if(cam)return inf;
-            else return 0;
-        }
-        if(root->left == NULL && root->right == NULL){
-            if(cam)
-                return 1;
-            else{
-                if(parCam)
-                    return 0;
-                else return inf;
-            }
-        }
-        
-        if(dp.find({root, {cam, parCam}}) != dp.end())
-            return dp[{root, {cam, parCam}}];
-        if(cam){
-            return dp[{root, {cam, parCam}}] = 1 + min(solve(root->left, 0, 1), solve(root->left, 1, 1)) +
-                        min(solve(root->right, 0, 1), solve(root->right, 1, 1));
-        }
-        else{
-            if(parCam){
-                return dp[{root, {cam, parCam}}] = min(solve(root->left, 0, 0), solve(root->left, 1, 0)) +
-                        min(solve(root->right, 0, 0), solve(root->right, 1, 0));
-            }
-            else{
-                int op1 = solve(root->left, 1, 0) + min(solve(root->right, 0, 0), solve(root->right, 1, 0));
-                int op2 = solve(root->right, 1, 0) + min(solve(root->left, 0, 0), solve(root->left, 1, 0));
-                return dp[{root, {cam, parCam}}] = min(op1, op2);
-            }
-        }
-    }
 public:
+    int needs=0;
+    int has=1;
+    int no=2;
+    int cover=3;
+    int cnt=0;
+    int helper(TreeNode* root){
+        if(!root){
+            return no;
+        }
+        if(root->left==NULL && root->right==NULL){
+            return needs;
+        }
+        int l=helper(root->left);
+        int r=helper(root->right);
+        if(l==needs || r==needs){
+            cnt++;
+            return has;
+        }
+        if(l==has || r==has){
+            return cover;
+        }
+        return needs;
+    }
     int minCameraCover(TreeNode* root) {
-        dp.clear();
-        return min(solve(root, 0, 0), solve(root, 1, 0));
+        cnt=helper(root)==needs?cnt+1:cnt;
+        return cnt;
     }
 };
