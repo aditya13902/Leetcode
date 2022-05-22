276bac5d6322cec433690bed42c20362426c9dab
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-22 10:19:34 +0530

    Time: 478 ms (66.48%), Space: 116.4 MB (48.60%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    map<pair<int,int>,int>mp;
     int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<int>>adj(n);
-        vector<unordered_map<int,int>>mp(n);
-        for(auto vec:connections){
-            adj[vec[0]].push_back(vec[1]);
-            adj[vec[1]].push_back(vec[0]);
-            mp[vec[0]][vec[1]]=1;
-        }
-        vector<int>vis(n,0);
-        queue<int>q;
+        vector<int> visited(n);
+        vector<vector<int>> adj(n), back(n);
+        queue<int> q;
         q.push(0);
-        int cnt=0;
-        while(q.size()){
-            int ele=q.front();
+        int ans = 0;
+        
+        for (auto c : connections){
+            adj[c[0]].push_back(c[1]);
+            back[c[1]].push_back(c[0]);
+        }
+        
+        while (!q.empty()){
+            int curr = q.front();
             q.pop();
-            vis[ele]=1;
-            for(auto v:adj[ele]){
-                if(vis[v]){
-                    continue;
+            visited[curr] = 1;
+
+            // change dir for all arrows facing out
+            for (auto a: adj[curr]){
+                if (!visited[a]){
+                    ans++;
+                    q.push(a);
                 }
-                if(mp[v][ele]==0){
-                    // cout<<v<<"-"<<ele<<endl;
-                    cnt++;
-                }
-                q.push(v);
-                
+            }
+            // push other nodes so we visit everything
+            for (auto b: back[curr]){
+                if (!visited[b]) q.push(b);
             }
         }
-        return cnt;
+        return ans;
     }
 };
