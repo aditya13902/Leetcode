fd77ea4a4441dc83493e88d530487859ff992762
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-22 19:01:25 +0530

    Time: 33 ms (31.11%), Space: 20.2 MB (18.18%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,26 +1,24 @@
 class Solution {
-    vector<vector<int>>vec;
 public:
-    int helper(string s,int i,int j){
-        if(i>=j){
-            return 1;
-        }
-        if(vec[i][j]!=-1){
-            return vec[i][j];
-        }
-        if(s[i]==s[j]){
-            return vec[i][j]=helper(s,i+1,j-1);
-        }
-        else{
-            return vec[i][j]=0;
-        }
-    }
     int countSubstrings(string s) {
+        vector<vector<int>>dp(s.size(),vector<int>(s.size(),0));
+        for(int i=0;i<s.size();i++){
+            dp[i][i]=1;
+        }
+        for(int l=2;l<=s.size();l++){
+            for(int i=0;i<=s.size()-l;i++){
+                int j=i+l-1;
+                if(s[i]==s[j] && (i+1==j || dp[i+1][j-1])){
+                    dp[i][j]=1;
+                }
+            }
+        }
         int cnt=0;
-        vec.resize(s.size(),vector<int>(s.size(),-1));
         for(int i=0;i<s.size();i++){
             for(int j=i;j<s.size();j++){
-                cnt+=helper(s,i,j);
+                if(dp[i][j]==1){
+                    cnt++;
+                }
             }
         }
         return cnt;
