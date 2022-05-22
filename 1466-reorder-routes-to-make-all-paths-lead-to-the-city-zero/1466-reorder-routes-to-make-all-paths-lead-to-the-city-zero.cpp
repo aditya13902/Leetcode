67e931cc7cd681d238a5c09251d23a16c2b5e549
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-22 10:18:37 +0530

    Time: 1083 ms (7.22%), Space: 183.9 MB (6.10%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -3,29 +3,30 @@ public:
     map<pair<int,int>,int>mp;
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<int>>adj(n);
+        vector<unordered_map<int,int>>mp(n);
         for(auto vec:connections){
             adj[vec[0]].push_back(vec[1]);
             adj[vec[1]].push_back(vec[0]);
-            mp[{vec[0],vec[1]}]=1;
+            mp[vec[0]][vec[1]]=1;
         }
         vector<int>vis(n,0);
         queue<int>q;
         q.push(0);
         int cnt=0;
-        vis[0]=1;
         while(q.size()){
             int ele=q.front();
             q.pop();
+            vis[ele]=1;
             for(auto v:adj[ele]){
                 if(vis[v]){
                     continue;
                 }
-                if(mp.find({v,ele})==mp.end()){
+                if(mp[v][ele]==0){
                     // cout<<v<<"-"<<ele<<endl;
                     cnt++;
                 }
                 q.push(v);
-                vis[v]=1;
+                
             }
         }
         return cnt;
