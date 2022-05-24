8fd93b685e3606aa7fe7bf8fc115f2dd473993e8
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-24 10:46:39 +0530

    Time: 21 ms (41.10%), Space: 13.4 MB (11.37%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_set<int>row;
-        unordered_set<int>col;
-        
+        unordered_map<int,int>mpx;
+        unordered_map<int,int>mpy;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
                 if(matrix[i][j]==0){
-                    row.insert(i);
-                    col.insert(j);
+                    mpx[i]=1;
+                    mpy[j]=1;
                 }
             }
         }
         for(int i=0;i<matrix.size();i++){
-            if(row.find(i)!=row.end()){
-                for(int j=0;j<matrix[0].size();j++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(mpx[i]){
                     matrix[i][j]=0;
                 }
-            }
-        }
-        for(int j=0;j<matrix[0].size();j++){
-            if(col.find(j)!=col.end()){
-                for(int i=0;i<matrix.size();i++){
+                if(mpy[j]){
                     matrix[i][j]=0;
                 }
             }
         }
     }
 };
-
- // [0,0,3,0],
- // [0,0,7,8],
- // [0,10,11,12],
- // [0,14,15,0]
