6a64e52c5c0967159516700e79dcd1409ab8802c
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-25 13:16:53 +0530

    Time: 42 ms (91.86%), Space: 18.8 MB (94.01%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool flag(vector<int>&vec,int h,int k){
+        int cnt=0;
+        for(auto ele:vec){
+            cnt+=ele/k;
+            if(ele%k!=0){
+                cnt++;
+            }
+        }
+        return cnt<=h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int s=1;
+        int e=1;
+        for(auto ele:piles){
+            e=max(ele,e);
+        }
+        while(s<e){
+            int m=s+(e-s)/2;
+            if(flag(piles,h,m)){
+                e=m;
+            }
+            else{
+                s=m+1;
+            }
+        }
+        return s;
+    }
+};
