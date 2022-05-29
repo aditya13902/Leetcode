b72bde5e5adff34935df0fc99f1225ab2e3f4074
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-29 22:46:29 +0530

    Time: 141 ms (81.95%), Space: 41.8 MB (81.01%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     vector<int>parent;
-    int getParent(int v){
-        if(v==parent[v]){
+    int find(int v){
+        if(parent[v]==v){
             return v;
         }
-        return getParent(parent[v]);
+        return parent[v]=find(parent[v]);
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        // int n=connections.size();
         if(connections.size()<n-1){
             return -1;
         }
-        parent.resize(n);
+        parent.resize(n,0);
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
-        int cnt=0;
         for(auto vec:connections){
-            int a=getParent(vec[0]);
-            int b=getParent(vec[1]);
+            int a=vec[0];
+            int b=vec[1];
+            a=find(a);
+            b=find(b);
             if(a!=b){
                 parent[a]=b;
             }
-            else{
+            
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(parent[i]==i){
                 cnt++;
             }
         }
-        int cnt2=0;
-        for(int i=0;i<n;i++){
-            if(parent[i]==i){
-                cnt2++;
-            }
-        }
-        return min(cnt,cnt2-1);
+        return cnt-1;
+        
     }
 };
