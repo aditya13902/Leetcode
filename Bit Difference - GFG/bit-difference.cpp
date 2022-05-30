63beb88798db1a9f2de9c16c01f0ce719cfd0285
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-30 11:24:32 +0530

    Added solution - LeetHub

---
+++ Bit Difference - GFG/bit-difference.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        
+        // Your logic here
+        if(a==0 && b==0){
+            return 0;
+        }
+        if((a&1) == (b&1)){
+            return countBitsFlip(a>>1 ,b>>1);
+        }
+        return 1+countBitsFlip(a>>1 ,b>>1);
+        
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;// input the testcases
+	while(t--) //while testcases exist
+	{
+		int a,b;
+		cin>>a>>b; //input a and b
+
+        Solution ob;
+		cout<<ob.countBitsFlip(a, b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
