1f588cb6e3524845207d85a4d1829580f8ea4021
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-05-31 22:14:19 +0530

    Time: 59 ms (8.85%), Space: 14.5 MB (27.01%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        int n=s.size();
-        vector<int>dp(n+1);
+        unordered_set<string>st;
+        for(auto str:wordDict){
+            st.insert(str);
+        }
+        vector<int>dp(s.size()+1,0);
         dp[0]=1;
-        for(int i=1;i<=n;i++){
-            for(auto w:wordDict){
-                if(i>=w.size()){
-                    string str=s.substr(i-w.size(),w.size());
-                    if(str==w && dp[i-w.size()]){
+        for(int i=1;i<=s.size();i++){
+            for(int j=0;j<i;j++){
+                string sub=s.substr(j,i-j);
+                if(st.find(sub)!=st.end()){
+                    // cout<<sub<<endl;
+                    if(dp[j]){
+                        // cout<<i<<endl;
                         dp[i]=1;
                     }
+                    
                 }
             }
         }
-        return dp[n];
+        return dp[s.size()];
     }
 };
