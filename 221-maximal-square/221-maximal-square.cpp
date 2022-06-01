d20b558f014ada1940df134ee3f7e28d9050d84d
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-06-01 22:10:44 +0530

    Time: 116 ms (25.79%), Space: 18.1 MB (38.48%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        vector<vector<int>>dp(matrix.size(),vector<int>(matrix[0].size()));
+        int row=matrix.size();
+        int col=matrix[0].size();
+        vector<vector<int>>dp(row,vector<int>(col,0));
         int ans=0;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(matrix[i][j]=='1'){
-                    dp[i][j]=1;
+                    // cout<<"yes"<<endl;
                     ans=1;
+                    dp[i][j]=1;
                 }
             }
         }
-        
-        for(int i=matrix.size()-2;i>=0 ;i--){
-            for(int j=matrix[0].size()-2;j>=0;j--){
+        for(int i=row-2;i>=0;i--){
+            for(int j=col-2;j>=0;j--){
                 if(dp[i][j]==1){
-                    dp[i][j]=1+min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]});
+                    // cout<<"yes"<<endl;
+                    dp[i][j]=1+min({dp[i+1][j],dp[i+1][j+1],dp[i][j+1]});
                 }
-                ans=max(dp[i][j],ans);
-                
+                ans=max(ans,dp[i][j]);
             }
         }
         return ans*ans;
+        
+        
     }
 };
