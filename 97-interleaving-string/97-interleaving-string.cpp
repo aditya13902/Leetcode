1dd61b319005d9e88f1a76eb701e9282095fbec3
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-06-01 22:50:32 +0530

    Time: 7 ms (49.41%), Space: 7.1 MB (43.01%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    bool helper(string s1,string s2,string s3,int i,int j,int k){
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(i==s1.size() && j==s2.size() && k==s3.size()){
-            return true;
-        }
-        if(i==s1.size()){
-            while(j<s2.size() && k<s3.size() && s2[j]==s3[k]){
-                j++;
-                k++;
-            }
-            return j==s2.size() && k==s3.size();
-        }
-        if(j==s2.size()){
-            while(i<s1.size() && k<s3.size() && s1[i]==s3[k]){
-                i++;
-                k++;
-            }
-            return i==s1.size() && k==s3.size();
-        }
-        bool tmp=false;
-        if(s1[i]==s3[k]){
-            tmp|=helper(s1,s2,s3,i+1,j,k+1);
-        }
-        if(s2[j]==s3[k]){
-            tmp|=helper(s1,s2,s3,i,j+1,k+1);
-        }
-        return dp[i][j]=tmp;
-    }
     bool isInterleave(string s1, string s2, string s3) {
+        
         int n=s1.size();
         int m=s2.size();
-        dp.resize(n+1,vector<int>(m+1,-1));
-        return helper(s1,s2,s3,0,0,0);
+        if(n+m !=s3.size()){
+            return false;
+        }
+        dp.resize(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else if(i==0){
+                    if(s2[j-1]==s3[i+j-1] && dp[i][j-1]){
+                        dp[i][j]=1;
+                    }
+                    else{
+                        dp[i][j]=0;
+                    }
+                }
+                else if(j==0){
+                    if(s1[i-1]==s3[i+j-1] && dp[i-1][j]){
+                        dp[i][j]=1;
+                    }
+                    else{
+                        dp[i][j]=0;
+                    }
+                }
+                else{
+                    if((s2[j-1]==s3[i+j-1] && dp[i][j-1]) || (s1[i-1]==s3[i+j-1] && dp[i-1][j])){
+                        dp[i][j]=1;
+                    }
+                    else{
+                        dp[i][j]=0;
+                    }
+                }
+            }
+        }
+        return dp[n][m];
+        
     }
 };
