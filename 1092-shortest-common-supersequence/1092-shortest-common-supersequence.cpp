1223872deacf44ce9fd2366fb4642bf232a2ac55
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-06-16 13:47:28 +0530

    Time: 40 ms (29.28%), Space: 15.3 MB (17.41%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        int n=str1.size();
-        int m=str2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(str1[i-1]==str2[j-1]){
+    string shortestCommonSupersequence(string text1, string text2) {
+        vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                if(text1[i-1]==text2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
@@ -14,34 +12,40 @@ public:
                 }
             }
         }
-        string ans="";
-        int i=n;
-        int j=m;
+        int i=text1.size();
+        int j=text2.size();
+        string str="";
         while(i>0 && j>0){
-            if(str1[i-1]==str2[j-1]){
-                ans+=str1[i-1];
+            if(text1[i-1]==text2[j-1]){
+                str=text1[i-1]+str;
                 i--;
                 j--;
-            }
-            else if(dp[i-1][j]>dp[i][j-1]){
-                ans+=str1[i-1];
-                i--;
             }
             else{
-                ans+=str2[j-1];
-                j--;
+                if(dp[i-1][j]>dp[i][j-1]){
+                    str=text1[i-1]+str;
+                    i--;
+                }
+                else{
+                    str=text2[j-1]+str;
+                    j--;
+                }
             }
         }
-        while(i>0){
-            ans+=str1[i-1];
-            i--;
-        }
         while(j>0){
-            ans+=str2[j-1];
+            str=text2[j-1]+str;
             j--;
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        while(i>0){
+            str=text1[i-1]+str;
+            i--;
+        }
+        return str;
         
     }
 };
+    // 0  c a b
+    // a
+    // b
+    // a      z
+    // c    y x
