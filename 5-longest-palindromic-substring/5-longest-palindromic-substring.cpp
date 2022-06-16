c56ee5a8034372c719927f81309dbb5e026edad1
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-06-16 17:43:40 +0530

    Time: 1479 ms (5.01%), Space: 386.1 MB (9.28%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s==""){
-            return s;
-        }
-        vector<vector<int>>dp(s.size(),vector<int>(s.size(),0));
-        for(int i=0;i<s.size();i++){
-            for(int j=0;j<s.size();j++){
-                if(i==j){
-                    dp[i][j]=1;
-                }
-            }
-        }
-        int ans=1;
+        int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
         string str="";
-        str+=s[0];
-        for(int l=2;l<=s.size();l++){
-            for(int i=s.size()-l;i>=0;i--){
-                int j=i+l-1;
+        int ans=1;
+        for(int i=0;i<n;i++){
+            dp[i][i]=1;
+        }
+        str.push_back(s[0]);
+        for(int l=1;l<n;l++){
+            for(int i=0;i<n-l;i++){
+                int j=i+l;
                 if(s[i]==s[j]){
                     if(i+1==j){
-                        dp[i][j]=2;
+                        dp[i][j]=1;
                     }
-                    else{
-                        if(dp[i+1][j-1]!=0){
-                            dp[i][j]=2+dp[i+1][j-1];
-                        }
+                    else if(dp[i+1][j-1]){
+                        dp[i][j]=1;
                     }
                 }
-                if(dp[i][j]>ans){
-                    ans=dp[i][j];
-                    str=s.substr(i,l);
+                else{
+                    dp[i][j]=0;
                 }
-                
+                if(dp[i][j] && j-i+1>ans){
+                    str=s.substr(i,j-i+1);
+                    // cout<<i<<" "<<j<<endl;
+                    ans=l+1;
+                }
             }
         }
         return str;
