d82e8a335f9bfa736e8d963a3e15787980bd2d52
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-06-26 13:06:06 +0530

    Time: 7 ms (17.78%), Space: 5.9 MB (62.25%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
+    int solve(int n){
+        int sum=0;
+        while(n>0){
+            int curr=n%10;
+            n=n/10;
+            sum+=(curr*curr);
+        }
+        return sum;
+    }
     bool isHappy(int n) {
         // int sum=0;
-        unordered_map<int,int>mp;
-        while(n!=1){
-            if(mp[n]!=0){
-                return false;
-            }
-            mp[n]=1;
-            int sum=0;
-            while(n>0){
-                int curr=n%10;
-                n=n/10;
-                sum+=curr*curr;
-            }
-            n=sum;
+        int slow=n;
+        int fast=n;
+        do{
             
-        }
-        return true;
+            slow=solve(slow);
+            fast=solve(solve(fast));
+            if(fast==1){
+                return true;
+            }
+        }while(slow!=fast);
+        return false;
     }
 };
