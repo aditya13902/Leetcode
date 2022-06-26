117d2b15a43cf29243915004a06aa804cbd5d488
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-06-26 13:08:47 +0530

    Time: 2 ms (64.53%), Space: 5.9 MB (83.83%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int solve(int n){
-        int sum=0;
-        while(n>0){
-            int curr=n%10;
-            n=n/10;
-            sum+=(curr*curr);
+    int next(int n)
+    {
+        int sum = 0;
+        
+        while(n != 0)
+        {
+            sum += pow(n % 10,2);
+            n = n / 10;
         }
+        
         return sum;
     }
+
+public:
     bool isHappy(int n) {
-        // int sum=0;
-        int slow=n;
-        int fast=n;
-        do{
-            
-            slow=solve(slow);
-            fast=solve(solve(fast));
-            if(fast==1){
-                return true;
-            }
-        }while(slow!=fast);
-        return false;
+        int slow = next(n);
+        int fast = next(next(n));
+        
+        while(slow != fast)
+        {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        
+        return fast == 1 ;
     }
 };
