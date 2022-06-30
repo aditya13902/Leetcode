ba4c3016c3ea62b5040d83b3e7d85706d5271443
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-06-30 08:09:36 +0530

    Time: 8 ms (99.96%), Space: 14 MB (50.06%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,36 +1,33 @@
-// 1->4;
-// 2->4;
-// 3->1;
-// 3->2;
 class Solution {
 public:
-
-    bool canFinish(int n, vector<vector<int>>& req) {
-        vector<vector<int>>adj(n);
-        for(int i=0;i<req.size();i++){
-            adj[req[i][1]].push_back(req[i][0]);
-        }
-        vector<int>indeg(n,0);
-        for(int i=0;i<n;i++){
-            for(auto it:adj[i]){
-                indeg[it]++;
-            }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indeg(numCourses,0);
+        for(auto vec:prerequisites){
+            adj[vec[1]].push_back(vec[0]);
+            indeg[vec[0]]++;
         }
         queue<int>q;
-        for(int i=0;i<n;i++){
+        int f=0;
+        for(int i=0;i<numCourses;i++){
             if(indeg[i]==0){
-                // cout<<ele<<" ";
+                f=1;
                 q.push(i);
             }
         }
+        if(!f){
+            return false;
+        }
         while(q.size()){
-            auto ele=q.front();
-            q.pop();
-            for(auto it:adj[ele]){
-                indeg[it]--;
-                if(indeg[it]==0){
-                    // cout<<it<<" ";
-                    q.push(it);
+            int l=q.size();
+            while(l--){
+                auto curr=q.front();
+                q.pop();
+                for(auto ele:adj[curr]){
+                    indeg[ele]--;
+                    if(indeg[ele]==0){
+                        q.push(ele);
+                    }
                 }
             }
         }
