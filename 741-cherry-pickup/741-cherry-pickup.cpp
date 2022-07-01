0a71fc0b9d9b485412913c320aebd6a19588a74d
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-01 10:25:50 +0530

    Time: 127 ms (47.46%), Space: 22.5 MB (57.80%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
+    int n;
     vector<vector<vector<int>>>dp;
     int helper(vector<vector<int>>& grid,int r1,int c1,int c2){
         int r2=r1+c1-c2;
-        if(r1>=grid.size() || r2>=grid.size() || c1>=grid[0].size() || c2>=grid[0].size() || grid[r1][c1]==-1 || grid[r2][c2]==-1){
+        if(r1>=n || r2>=n || c1>=n || c2>=n || grid[r1][c1]==-1 || grid[r2][c2]==-1){
             return INT_MIN;
         }
         if(dp[r1][c1][c2]!=-1){
             return dp[r1][c1][c2];
         }
-        if(r1==grid.size()-1 && c1==grid.size()-1){
+        if(r1==n-1 && c1==n-1){
             return grid[r1][c1];
         }
-        
         int ans=grid[r1][c1];
         if(r1!=r2){
             ans+=grid[r2][c2];
         }
-        int pick=max(helper(grid,r1,c1+1,c2+1),helper(grid,r1+1,c1,c2));
-        pick=max(pick,helper(grid,r1+1,c1,c2+1));
+        int pick =max(helper(grid,r1+1,c1,c2+1),helper(grid,r1+1,c1,c2));
+        pick=max(pick,helper(grid,r1,c1+1,c2+1));
         pick=max(pick,helper(grid,r1,c1+1,c2));
         return dp[r1][c1][c2]=pick+ans;
     }
     int cherryPickup(vector<vector<int>>& grid) {
-        int n=grid.size();
+        n=grid.size();
         dp.resize(n,vector<vector<int>>(n,vector<int>(n,-1)));
-        // memset(dp,-1,sizeof(dp));
         return max(0,helper(grid,0,0,0));
     }
 };
