abcbcde2869f4f7c89dd53df3d0edf107ea34558
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-02 13:47:49 +0530

    Time: 44 ms (5.40%), Space: 16.4 MB (5.24%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>>q;
+        int time=0;
+        queue<vector<int>>q;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==2){
@@ -9,45 +10,38 @@ public:
                 }
             }
         }
-        int t=0;
         while(q.size()){
-            int l=q.size();
-            while(l--){
-                auto top=q.front();
+            int len=q.size();
+            // cout<<len<<endl;
+            while(len--){
+                auto curr=q.front();
                 q.pop();
-                int r=top.first;
-                int c=top.second;
-                if(r+1<grid.size() && grid[r+1][c]==1){
-                    grid[r+1][c]=2;
-                    q.push({r+1,c});
+                int x=curr[0];
+                int y=curr[1];
+                if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size()){
+                    continue;
                 }
-                if(r-1>=0 && grid[r-1][c]==1){
-                    grid[r-1][c]=2;
-                    q.push({r-1,c});
+                if(grid[x][y]==-1 || grid[x][y]==0){
+                    continue;
                 }
-                if(c-1>=0 && grid[r][c-1]==1){
-                    grid[r][c-1]=2;
-                    q.push({r,c-1});
-                }
-                if(c+1<grid[0].size() && grid[r][c+1]==1){
-                    grid[r][c+1]=2;
-                    q.push({r,c+1});
-                }
-                
+                grid[x][y]=-1;
+                q.push({x+1,y});
+                q.push({x-1,y});
+                q.push({x,y+1});
+                q.push({x,y-1});
             }
             if(q.size()){
-                t++;
+                time++;
             }
-            // t++;
-            
         }
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==1){
+                    // cout<<i<<" "<<j<<endl;
                     return -1;
                 }
             }
         }
-        return t;
+        return time==0?0:time-1;
     }
 };
