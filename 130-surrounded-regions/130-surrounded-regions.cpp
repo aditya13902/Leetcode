cde164c4009afb4aaeb120fb70d25d37c75534d7
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-02 13:05:42 +0530

    Time: 19 ms (56.48%), Space: 10 MB (49.26%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void helper(vector<vector<char>>& board,int i,int j){
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size()){
+            return;
+        }
+        if(board[i][j]!='O'){
+            return;
+        }
+        board[i][j]='-';
+        helper(board,i+1,j);
+        helper(board,i,j+1);
+        helper(board,i-1,j);
+        helper(board,i,j-1);
+    }
+    void solve(vector<vector<char>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O' && (i==0 || j==0 || i==n-1 ||j==m-1)){
+                    helper(board,i,j);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                board[i][j]=board[i][j]=='-'?'O':'X';
+            }
+        }
+    }
+};
