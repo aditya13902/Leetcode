2371c09368a00395f2001a865083815cf8b1230f
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-04 22:11:08 +0530

    Time: 98 ms (88.81%), Space: 87.5 MB (10.99%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(auto ele:nums){
-            sum+=ele;
-        }
-        if(sum%2){
+    vector<vector<int>>dp;
+    bool divide(vector<int>&nums,int tot,int idx){
+        if(tot==0) return true;
+        if(idx==nums.size()){
             return false;
         }
-        sum/=2;
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                if(j==0){
-                    dp[i][j]=1;
-                }
-                else if(i==0){
-                    dp[i][j]=0;
-                }
-            }
+        if(tot<0){
+            return false;
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=sum;j++){
-                if(j>=nums[i-1]){
-                    dp[i][j]=dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                }
-                else{
-                    dp[i][j]= dp[i-1][j];
-                }
-                
-            }
+        if(dp[tot][idx]!=-1){
+            return dp[tot][idx];
         }
-        return dp[n][sum];
+        return dp[tot][idx]=divide(nums,tot-nums[idx],idx+1) || divide(nums,tot,idx+1);
+    }
+    bool canPartition(vector<int>& nums) {
+        int tot=0;
+        for(auto ele:nums){
+            tot+=ele;
+        }
+        if(tot%2){
+            return false;
+        }
+        dp.resize((tot/2)+1,vector<int>(nums.size(),-1));
+        return divide(nums,tot/2,0);
+        
     }
 };
