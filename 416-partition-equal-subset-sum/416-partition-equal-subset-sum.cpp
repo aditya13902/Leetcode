0e9b070ef7badeb770bd2e5dc4433ece00b132a6
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-04 22:37:11 +0530

    Time: 508 ms (20.28%), Space: 152.8 MB (6.11%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -24,23 +24,29 @@ public:
         }
         dp.resize((tot/2)+1,vector<int>(nums.size(),0));
         // return divide(nums,tot/2,0);
-        for(int i=0;i<nums.size();i++){
-            dp[0][i]=1;
-        }
+        // for(int i=0;i<nums.size();i++){
+        //     dp[0][i]=1;
+        // }
         int sum=tot/2;
+        // if(nums[0]<=sum){
+        //     dp[nums[0]][0]=1;
+        // }
+        vector<int>prev(sum+1,0);
+        prev[0]=1;
         if(nums[0]<=sum){
-            dp[nums[0]][0]=1;
+            prev[nums[0]]=1;
         }
-        
         for(int i=1;i<nums.size();i++){
+            vector<int>curr(sum+1,0);
             for(int j=1;j<=sum;j++){
-                dp[j][i]=dp[j][i-1];
+                curr[j]=prev[j];
                 if(j-nums[i]>=0){
-                    dp[j][i]|=dp[j-nums[i]][i-1];
+                    curr[j]|=prev[j-nums[i]];
                 }
             }
+            prev=curr;
         }
-        return dp[tot/2][nums.size()-1];
+        return prev[sum];
         
     }
 };
