fff387f295711a948c1f59b6a75dd3bafdb45c53
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-04 14:03:51 +0530

    Time: 21 ms (92.11%), Space: 18.2 MB (33.66%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         vector<int>candyl(ratings.size(),1);
-        for(int i=1;i<ratings.size();i++){
-            if(ratings[i-1]<ratings[i]){
-                candyl[i]=candyl[i-1]+1;
+        for(int i=0;i<ratings.size()-1;i++){
+            if(ratings[i]<ratings[i+1]){
+                candyl[i+1]=candyl[i]+1;
             }
         }
         vector<int>candyr(ratings.size(),1);
