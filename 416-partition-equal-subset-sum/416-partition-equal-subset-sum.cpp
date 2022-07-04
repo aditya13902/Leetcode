ae03081930de2df9272386bcc5a150ea2eeb7a99
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-04 22:29:46 +0530

    Time: 708 ms (8.85%), Space: 87.5 MB (11.49%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    bool divide(vector<int>&nums,int tot,int idx){
-        if(tot==0) return true;
-        if(idx==nums.size()){
-            return false;
-        }
-        if(tot<0){
-            return false;
-        }
-        if(dp[tot][idx]!=-1){
-            return dp[tot][idx];
-        }
-        return dp[tot][idx]=divide(nums,tot-nums[idx],idx+1) || divide(nums,tot,idx+1);
-    }
+    // bool divide(vector<int>&nums,int tot,int idx){
+    //     if(tot==0) return true;
+    //     if(idx==nums.size()){
+    //         return false;
+    //     }
+    //     if(tot<0){
+    //         return false;
+    //     }
+    //     if(dp[tot][idx]!=-1){
+    //         return dp[tot][idx];
+    //     }
+    //     return dp[tot][idx]=divide(nums,tot-nums[idx],idx+1) || divide(nums,tot,idx+1);
+    // }
     bool canPartition(vector<int>& nums) {
         int tot=0;
         for(auto ele:nums){
@@ -22,8 +22,25 @@ public:
         if(tot%2){
             return false;
         }
-        dp.resize((tot/2)+1,vector<int>(nums.size(),-1));
-        return divide(nums,tot/2,0);
+        dp.resize((tot/2)+1,vector<int>(nums.size(),0));
+        // return divide(nums,tot/2,0);
+        for(int i=0;i<nums.size();i++){
+            dp[0][i]=1;
+        }
+        int sum=tot/2;
+        if(nums[0]<=sum){
+            dp[nums[0]][0]=1;
+        }
+        
+        for(int i=1;i<nums.size();i++){
+            for(int j=1;j<=sum;j++){
+                dp[j][i]=dp[j][i-1];
+                if(j-nums[i]>=0){
+                    dp[j][i]|=dp[j-nums[i]][i-1];
+                }
+            }
+        }
+        return dp[tot/2][nums.size()-1];
         
     }
 };
