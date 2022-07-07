e5f694c977dcf32f3d60428ed38b7aae6d25b821
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-07 10:28:59 +0530

    Time: 77 ms (13.15%), Space: 57.9 MB (68.97%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,36 +11,34 @@
  */
 class Solution {
 public:
-    vector<int>store;
-    unordered_map<int,TreeNode*>mp;
+    TreeNode* prev=NULL;
+    TreeNode* first=NULL;
+    TreeNode* middle=NULL;
+    TreeNode* second=NULL;
     void inorder(TreeNode* root){
-        if(!root) return;
+        if(!root) return; 
+        
         inorder(root->left);
-        store.push_back(root->val);
-        mp[root->val]=root;
+        if(prev && root->val<prev->val){
+            if(!first){
+                first=prev;
+                middle=root;
+            }
+            else{
+                second=root;
+            }
+        }
+        prev=root;
         inorder(root->right);
     }
     void recoverTree(TreeNode* root) {
         inorder(root);
-        TreeNode* first=NULL;
-        TreeNode* middle=NULL;
-        TreeNode* second=NULL;
-        for(int i=0;i<store.size()-1;i++){
-            if(store[i]>store[i+1]){
-                if(first==nullptr){
-                    first=mp[store[i]];
-                    middle=mp[store[i+1]];
-                }
-                else{
-                    second=mp[store[i+1]];
-                }
-            }
-        }
         if(second){
             swap(first->val,second->val);
         }
         else{
             swap(first->val,middle->val);
         }
+        
     }
 };
