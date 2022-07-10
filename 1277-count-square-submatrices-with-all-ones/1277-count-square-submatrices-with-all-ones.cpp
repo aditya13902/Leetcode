e4abe43884c62f885f67170f9673dea03a22da09
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-10 10:09:21 +0530

    Time: 125 ms (33.43%), Space: 26.2 MB (47.79%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -3,35 +3,42 @@ public:
     int countSquares(vector<vector<int>>& matrix) {
         int r=matrix.size();
         int c=matrix[0].size();
-        vector<vector<int>>dp(r,vector<int>(c,0));
+        // vector<vector<int>>dp(r,vector<int>(c,0));
         int cnt=0;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==1){
-                    dp[i][j]=1;
-                }
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix[0].size();j++){
+        //         if(matrix[i][j]==1){
+        //             dp[i][j]=1;
+        //         }
+        //     }
+        // }
+        vector<int>prev(c);
+        for(int i=0;i<c;i++){
+            if(matrix[r-1][i]){
+                prev[i]=1;
+                cnt++;
             }
         }
-        
         int ans=0;
         for(int i=r-2;i>=0;i--){
+            vector<int>curr(c);
+            if(matrix[i][c-1]){
+                curr[c-1]=1;
+                cnt++;
+            }
             for(int j=c-2;j>=0;j--){
                 if(matrix[i][j]){
-                    dp[i][j]=1+min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]});
+                    curr[j]=1+min({prev[j+1],prev[j],curr[j+1]});
                     
                 }
                 else{
-                    dp[i][j]=0;
+                    curr[j]=0;
                 }
+                cnt+=curr[j];
             }
+            prev=curr;
         }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                // cout<<dp[i][j]<<" ";
-                cnt+=dp[i][j];
-            }
-            // cout<<e/ndl;
-        }
+
         return cnt;
     }
 };
