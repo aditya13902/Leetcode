8e0889e55da8fc827463bf5bcdfe0ad5d2a42b5e
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-12 22:03:37 +0530

    Time: 2 ms (53.06%), Space: 6.1 MB (56.33%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int dp[101][101];
-    bool dfs(string &s, int i, int open) {
-        if (open < 0) return 0;
-        if (i == s.length()) return open == 0;
-        if (dp[i][open] != -1) return dp[i][open];
-        if (s[i] == '(') return dp[i][open] = dfs(s, i + 1, open + 1);
-        if (s[i] == ')') return dp[i][open] = dfs(s, i + 1, open - 1);      
-        return dp[i][open] = dfs(s, i + 1, open + 1) || dfs(s, i + 1, open - 1) || dfs(s, i + 1, open);
-    }
-    
     bool checkValidString(string s) {
-        memset(dp, -1, sizeof(dp));
-        return dfs(s, 0, 0);
+        int currmx=0;
+        int currmn=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                currmn++;
+                currmx++;
+            }
+            else if(s[i]==')'){
+                currmn--;
+                currmx--;
+            }
+            else{
+                currmx++;
+                currmn--;
+            }
+            if(currmx<0) return false;
+            currmn=max(0,currmn);
+        }
+        return currmn==0;
     }
 };
