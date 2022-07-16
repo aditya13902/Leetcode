a2f19704cce7250f67971abef1f11b6bdc6122e8
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-16 10:41:08 +0530

    Time: 97 ms (36.46%), Space: 27 MB (44.29%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,51 +1,23 @@
 class Solution {
 public:
-    int minInsertions(string str) {
-        int n=str.size();
-        vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
-        for(int i=0;i<n;i++){
-            dp[i][i]=0;
-        }
-        for(int l=1;l<n;l++){
-            for(int i=0;i<n-l;i++){
-                int j=i+l;
-                if(str[i]==str[j]){
-                    if(i==j-1){
-                        dp[i][j]=0;
-                    }
+    int minInsertions(string s) {
+        vector<vector<int>>dp(s.size(),vector<int>(s.size(),0));
+        for(int i=0;i<s.size();i++) dp[i][i]=1;
+        for(int l=2;l<=s.size();l++){
+            for(int i=0;i<=s.size()-l;i++){
+                int j=i+l-1;
+                if(s[i]==s[j]){
+                    if(l==2) dp[i][j]=2;
                     else{
-                        dp[i][j]=dp[i+1][j-1];
+                        dp[i][j]=2+dp[i+1][j-1];
                     }
                 }
                 else{
-                    dp[i][j]=1+min(dp[i+1][j],dp[i][j-1]);
+                    dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
                 }
-            }     
+            }
         }
-        return dp[0][n-1];
+        int n=s.size();
+        return s.size()-dp[0][n-1];
     }
 };
-// mbadm
-// // mdabm
-//     int n=str.size();
-//     vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
-//     for(int i=0;i<n;i++){
-//         dp[i][i]=0;
-//     }
-//     for(int l=1;l<n;l++){
-//         for(int i=0;i<n-l;i++){
-//             int j=i+l;
-//             if(str[i]==str[j]){
-//                 if(i==j-1){
-//                     dp[i][j]=0;
-//                 }
-//                 else{
-//                     dp[i][j]=dp[i+1][j-1];
-//                 }
-//             }
-//             else{
-//                 dp[i][j]=1+min(dp[i+1][j],dp[i][j-1]);
-//             }
-//         }     
-//     }
-//     return dp[0][n-1];
