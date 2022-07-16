9fbcf0aee58b743bdee3224a034b865d046b2a99
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-16 12:13:42 +0530

    Time: 89 ms (70.48%), Space: 26.4 MB (27.20%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
-        int r=matrix.size();
-        int c=matrix[0].size();
-        // vector<vector<int>>dp(r,vector<int>(c,0));
-        int cnt=0;
-        // for(int i=0;i<matrix.size();i++){
-        //     for(int j=0;j<matrix[0].size();j++){
-        //         if(matrix[i][j]==1){
-        //             dp[i][j]=1;
-        //         }
-        //     }
-        // }
-        vector<int>prev(c);
-        for(int i=0;i<c;i++){
-            if(matrix[r-1][i]){
-                prev[i]=1;
-                cnt++;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dp[i][j]=matrix[i][j];
             }
         }
         int ans=0;
-        for(int i=r-2;i>=0;i--){
-            vector<int>curr(c);
-            if(matrix[i][c-1]){
-                curr[c-1]=1;
-                cnt++;
-            }
-            for(int j=c-2;j>=0;j--){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(matrix[i][j]){
-                    curr[j]=1+min({prev[j+1],prev[j],curr[j+1]});
-                    
+                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
                 }
                 else{
-                    curr[j]=0;
+                    dp[i][j]=0;
                 }
-                cnt+=curr[j];
             }
-            prev=curr;
         }
-
-        return cnt;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans+=dp[i][j];
+            }
+        }
+        return ans;
     }
 };
