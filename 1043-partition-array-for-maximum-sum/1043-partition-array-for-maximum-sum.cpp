44b50e8d36b5e5fd465f712b8d1df175de226f31
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-17 13:45:21 +0530

    Time: 39 ms (22.80%), Space: 8.5 MB (82.43%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,18 +1,17 @@
 class Solution {
-private:
-    vector<int>dp;
-    int helper(vector<int>&arr,int i,int k){
-        if(i==arr.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int mx=-1;
-        for(int j=i;j<arr.size() && j<i+k;j++){
-            mx=max(mx,arr[j]);
-            dp[i]=max(dp[i],(j-i+1)*mx+helper(arr,j+1,k));
-        }
-        return dp[i];
-    }
 public:
-    
+    vector<int>dp;
+    int helper(vector<int>&arr,int idx,int k){
+        if(idx==arr.size()) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int mx=0;
+        int ans=0;
+        for(int i=idx;i<idx+k && i<arr.size();i++){
+            mx=max(mx,arr[i]);
+            ans=max(ans,mx*(i-idx+1)+helper(arr,i+1,k));
+        }
+        return dp[idx]=ans;
+    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         dp.resize(arr.size(),-1);
         return helper(arr,0,k);
