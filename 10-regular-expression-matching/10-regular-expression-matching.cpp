50c927cafda3943b3f47170333df658c740542cc
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-20 11:13:24 +0530

    Time: 30 ms (31.15%), Space: 21.6 MB (6.60%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,26 +1,59 @@
 class Solution {
-public:
-    int dp[50][50];
-    bool memo(string s,string p,int i,int j){
-        if(i>=s.size() && j>=p.size()) return true;
-        if(j>=p.size()) return false;
-        if(dp[i][j]!=-1) return dp[i][j];
-        bool flag=(i<s.size() && (s[i]==p[j] || p[j]=='.'));
+    public:
+    
+    bool helper(string str, string pat, int i, int j, vector<vector<int>> &dp){
         
-        if(j+1<p.size() && p[j+1]=='*'){
-            return dp[i][j]= memo(s,p,i,j+2) ||(flag && memo(s,p,i+1,j));
+        if(i >= str.size() && j >= pat.size()){
+            return true;
         }
-        if (flag){
-            return dp[i][j]=memo(s,p,i+1,j+1);
+       
+        if(j >= pat.size()){
+            return false;
+        } 
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
-        return false;
-    }
-    bool isMatch(string s, string p) {
-        for(int i=0;i<50;i++){
-            for(int j=0;j<50;j++){
-                dp[i][j]=-1;
+        
+        if(i >= str.size()){
+            
+            if(pat[j + 1] == '*'){
+                
+                return dp[i][j] = helper(str, pat, i, j + 2, dp);
             }
+            
+            return false;
         }
-        return memo(s,p,0,0);
+        
+        if(j + 1 < pat.size() && pat[j + 1] == '*'){
+            
+              if(pat[j] == '.' || pat[j] == str[i])
+              {
+                  return dp[i][j] = (helper(str, pat, i + 1, j, dp) || helper(str, pat, i, j + 2, dp));
+              }
+            
+              else{
+                  
+                  return dp[i][j] = helper(str, pat, i, j + 2, dp);
+              }   
+        }
+        
+        else if (pat[j] == '.' || pat[j] == str[i])
+        {
+            return dp[i][j] = helper(str, pat, i + 1, j + 1, dp);
+        }
+        
+        return dp[i][j] = false;   
+    }
+    
+    bool isMatch(string str, string pat) {
+        
+        int n = str.size();
+        
+        int m = pat.size();
+        
+        vector<vector<int> > dp(n + 1, vector<int> (m + 1, -1));
+        
+        return helper(str, pat, 0, 0, dp) ? true : false;
     }
 };
