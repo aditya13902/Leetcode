3c4a99e01ad55cca70a9708394bfd23df4b630af
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-20 11:07:51 +0530

    Time: 627 ms (5.01%), Space: 24.4 MB (5.07%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[1000][1000];
+    bool memo(string s,string p,int i,int j){
+        if(i>=s.size() && j>=p.size()) return true;
+        if(j>=p.size()) return false;
+        if(dp[i][j]!=-1) return dp[i][j];
+        bool flag=(i<s.size() && (s[i]==p[j] || p[j]=='.'));
+        
+        if(j+1<p.size() && p[j+1]=='*'){
+            return dp[i][j]= memo(s,p,i,j+2) ||(flag && memo(s,p,i+1,j));
+        }
+        if (flag){
+            return dp[i][j]=memo(s,p,i+1,j+1);
+        }
+        return false;
+    }
+    bool isMatch(string s, string p) {
+        for(int i=0;i<1000;i++){
+            for(int j=0;j<1000;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return memo(s,p,0,0);
+    }
+};
