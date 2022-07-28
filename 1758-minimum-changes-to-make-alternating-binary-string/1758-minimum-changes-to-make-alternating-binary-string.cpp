ad0187a5a494a2f7c9a5c711971cf55d3a7cc106
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-28 11:11:29 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (16.84%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -3,12 +3,8 @@ public:
     int helper(string &s,int i,char prev){
         if(i>=s.size()) return 0;
         if(s[i]==prev){
-            if(prev=='0'){
-                return 1+helper(s,i+1,'1');
-            }
-            else{
-                return 1+helper(s,i+1,'0');
-            }
+            char opp=(s[i]=='0')?'1':'0';
+            return 1+helper(s,i+1,opp);
         }
         else{
             return helper(s,i+1,s[i]);
@@ -17,8 +13,8 @@ public:
     int minOperations(string s) {
         int ans=s.size();
         ans=min(ans,helper(s,1,s[0]));
-        char op=(s[0]=='0')?'1':'0';
-        ans=min(ans,1+helper(s,1,op));
+        char opp=(s[0]=='0')?'1':'0';
+        ans=min(ans,1+helper(s,1,opp));
         return ans;
     }
 };
