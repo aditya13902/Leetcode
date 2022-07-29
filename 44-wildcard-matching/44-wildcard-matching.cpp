3435ada357856a8bacdf9c6374bcd97fb7d72d09
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-07-30 00:00:01 +0530

    Time: 120 ms (33.08%), Space: 25.9 MB (42.26%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    bool helper(string &s,string &p,int i,int j){
-        if(i==s.size() && j==p.size()) return true;
-        if(j==p.size()) return false;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if( i<s.size() && (s[i]==p[j] || p[j]=='?')){
-            return dp[i][j]=helper(s,p,i+1,j+1);
-        }
-        else if(p[j]=='*'){
-            return dp[i][j]=(i<s.size() && helper(s,p,i+1,j)) || helper(s,p,i,j+1);
-        }
-        return false;
-    }
     bool isMatch(string s, string p) {
-        dp.resize(s.size()+1,vector<int>(p.size()+1,-1));
-        return helper(s,p,0,0);
+        
+        // vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,0));
+        vector<int>prev(p.size()+1,0);
+        for(int i=1;i<=p.size();i++){
+            if(p[i-1]!='*') break;
+            prev[i]=1;
+        }
+        prev[0]=1;
+        for(int i=1;i<=s.size();i++){
+            vector<int>curr(p.size()+1,0);
+            for(int j=1;j<=p.size();j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                    // dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
+                }
+                else if(p[j-1]=='*'){
+                    // dp[i][j]=dp[i][j-1]||dp[i-1][j];
+                    curr[j]=curr[j-1] || prev[j];
+                }
+            }
+            prev=curr;
+        }
+        return prev[p.size()];
     }
 };
