a5ee9e4c7018ff5b4f83415f030d94ad2643fb4a
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-02 22:29:13 +0530

    Time: 574 ms (17.17%), Space: 76.4 MB (13.49%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    bool helper(vector<int>&nums,int sum,int idx){
-        if(sum==0) return true;
-        if(sum<0) return false;
-        if(idx==nums.size()) return false;
-        
-        if(dp[idx][sum]!=-1) return dp[idx][sum];
-        return dp[idx][sum]=helper(nums,sum-nums[idx],idx+1) || helper(nums,sum,idx+1);
-    }
     bool canPartition(vector<int>& nums) {
-        int tot=0;
-        for(auto ele:nums){
-            tot+=ele;
+        int sum=0;
+        for(auto ele:nums) sum+=ele;
+        if(sum%2 || nums.size()<2) return false;
+        int n=nums.size();
+        int curr=sum/2;
+        vector<vector<int>>dp(n+1,vector<int>(curr+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=curr;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j-nums[i-1]>=0){
+                    dp[i][j]|=dp[i-1][j-nums[i-1]];
+                }
+            }
         }
-        if(tot%2){
-            return false;
-        }
-        int sum=tot/2;
-        dp.resize(nums.size(),vector<int>(sum+1,-1));
-        return helper(nums,sum,0);
-        
-        
+        return dp[n][curr];
     }
 };
