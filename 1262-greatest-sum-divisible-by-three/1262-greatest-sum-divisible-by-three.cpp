0fed9e7cca168428a2aa446523e0e9cb25f95f25
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-03 09:15:58 +0530

    Time: 121 ms (34.77%), Space: 41.6 MB (14.59%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,23 +1,16 @@
-
 class Solution {
 public:
     vector<vector<int>>dp;
-    int rec(vector<int>&nums,int idx,int curr){
-        if(idx==nums.size()){
-            if(curr==0){
-                return 0;
-            }
-            return INT_MIN;
-        }
+    int helper(vector<int>&nums,int idx,int curr){ // curr=curr_sum%3
+        if(idx==nums.size()) return curr==0?0:INT_MIN;
         if(dp[idx][curr]!=-1) return dp[idx][curr];
-        int a,b;
-        a=rec(nums,idx+1,curr);
-        b=nums[idx]+rec(nums,idx+1,(curr+nums[idx])%3);
-        return dp[idx][curr]=max(a,b);
+        int op1=nums[idx]+helper(nums,idx+1,(curr+nums[idx])%3);
+        int op2=helper(nums,idx+1,curr);
+        return dp[idx][curr]=max(op1,op2);
+        
     }
     int maxSumDivThree(vector<int>& nums) {
-        
         dp.resize(nums.size(),vector<int>(3,-1));
-        return rec(nums,0,0);
+        return helper(nums,0,0);
     }
 };
