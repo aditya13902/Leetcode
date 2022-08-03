b6f014ccb93b36cedeeee5dcc9d501894e075873
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-03 09:35:36 +0530

    Time: 173 ms (14.89%), Space: 38.3 MB (34.57%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int helper(vector<int>&nums,int idx,int curr){ // curr=curr_sum%3
-        if(idx==nums.size()) return curr==0?0:INT_MIN;
-        if(dp[idx][curr]!=-1) return dp[idx][curr];
-        int op1=nums[idx]+helper(nums,idx+1,(curr+nums[idx])%3);
-        int op2=helper(nums,idx+1,curr);
-        return dp[idx][curr]=max(op1,op2);
+//     int helper(vector<int>&nums,int idx,int curr){ // curr=curr_sum%3
+//         if(idx==nums.size()) return curr==0?0:INT_MIN;
+//         if(dp[idx][curr]!=-1) return dp[idx][curr];
+//         int op1=nums[idx]+helper(nums,idx+1,(curr+nums[idx])%3);
+//         int op2=helper(nums,idx+1,curr);
+//         return dp[idx][curr]=max(op1,op2);
         
-    }
+//     }
     int maxSumDivThree(vector<int>& nums) {
-        dp.resize(nums.size(),vector<int>(3,-1));
-        return helper(nums,0,0);
+        dp.resize(nums.size()+1,vector<int>(3,-1));
+        // return helper(nums,0,0);
+        dp[0][0]=0;
+        dp[0][1]=INT_MIN;
+        dp[0][2]=INT_MIN;
+        int n=nums.size();
+        for(int i=1;i<=n;i++){
+            dp[i][0]=max(dp[i-1][0],dp[i-1][(nums[i-1])%3]+nums[i-1]);
+            dp[i][1]=max(dp[i-1][1],dp[i-1][(nums[i-1]+1)%3]+nums[i-1]);
+            dp[i][2]=max(dp[i-1][2],dp[i-1][(nums[i-1]+2)%3]+nums[i-1]);
+        }
+        return dp[n][0];
     }
 };
