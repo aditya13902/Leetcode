040d359376cfad2aab057568e6852ef43558d3d2
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-04 14:36:42 +0530

    Time: 52 ms (53.18%), Space: 14.5 MB (74.14%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -4,10 +4,10 @@ public:
         vector<int>dp(n+1,1e8);
         dp[0]=0;
         for(int i=0;i<ranges.size();i++){
-            int lft=max(0,i-ranges[i]);
-            int rgt=min(n,i+ranges[i]);
-            for(int j=lft;j<=rgt;j++){
-                dp[j]=min(dp[j],1+dp[lft]);
+            int l=max(0,i-ranges[i]);
+            int r=min(n,i+ranges[i]);
+            for(int j=l;j<=r;j++){
+                dp[j]=min(dp[j],1+dp[l]);
             }
         }
         return dp[n]==1e8?-1:dp[n];
