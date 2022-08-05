1ff607df762cf0eeff61522bc008be996f541c76
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-05 16:07:58 +0530

    Time: 1724 ms (16.78%), Space: 253.5 MB (13.68%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-        int r=matrix.size();
-        int c=matrix[0].size();
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(j>0){
-                    matrix[i][j]+=matrix[i][j-1];
-                }
+        for(int i=0;i<matrix.size();i++){
+            int sum=0;
+            for(int j=0;j<matrix[0].size();j++){
+                sum+=matrix[i][j];
+                matrix[i][j]=sum;
             }
         }
-        int cnt=0;
-        for(int c1=0;c1<c;c1++){
-            for(int c2=c1;c2<c;c2++){
+        int ans=0;
+        for(int c1=0;c1<matrix[0].size();c1++){
+            for(int c2=c1;c2<matrix[0].size();c2++){
                 unordered_map<int,int>mp;
-                mp[0]=1;
                 int sum=0;
-                for(int i=0;i<r;i++){
-                    sum+=matrix[i][c2]-(c1>0?matrix[i][c1-1]:0);
-                    if(mp[sum-target]>0){
-                        cnt+=mp[sum-target];
-                    }
+                mp[0]=1;
+                for(int r=0;r<matrix.size();r++){
+                    sum+=matrix[r][c2]-((c1==0)?0:matrix[r][c1-1]);
+                    // ans+=mp[sum-target]+mp[target-sum];
+                    // sum+=matrix[r][c2]-(c1>0?matrix[r][c1-1]:0);
+                    if(mp[sum-target]) ans+=mp[sum-target];
                     mp[sum]++;
                 }
             }
         }
-        return cnt;
+        return ans;
     }
 };
