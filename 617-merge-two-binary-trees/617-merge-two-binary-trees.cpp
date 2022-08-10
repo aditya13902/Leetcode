6f627522a221de1ee7381bd6fd67b8aafb12c9ca
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-11 00:10:13 +0530

    Time: 64 ms (47.97%), Space: 32.2 MB (98.43%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -15,9 +15,9 @@ public:
         if(!root1 || !root2){
             return (!root1)?root2:root1;
         }
-        TreeNode* root=new TreeNode(root1->val+root2->val);
-        root->left=mergeTrees(root1->left,root2->left);
-        root->right=mergeTrees(root1->right,root2->right);
-        return root;
+        root1->val+=root2->val;
+        root1->left=mergeTrees(root1->left,root2->left);
+        root1->right=mergeTrees(root1->right,root2->right);
+        return root1;
     }
 };
