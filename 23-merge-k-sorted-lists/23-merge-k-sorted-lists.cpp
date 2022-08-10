f41d0211a7d058ca43c289025934f3f7a2a8aca6
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-10 13:51:51 +0530

    Time: 30 ms (78.98%), Space: 13.3 MB (75.86%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,25 +8,31 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class cmp{
+public:
+    bool operator()(ListNode* head1,ListNode* head2){
+        return head1->val>head2->val;
+    }
+};
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(ListNode* head:lists){
-            while(head!=NULL){
-                pq.push(head->val);
-                head=head->next;
+        priority_queue<ListNode*,vector<ListNode*>,cmp>pq;
+        for(auto node:lists){
+            if(!node) continue;
+            pq.push(node);
+        }
+        ListNode* dummy=new ListNode(-1);
+        auto ptr=dummy;
+        while(pq.size()){
+            auto head=pq.top();
+            pq.pop();
+            ptr->next=head;
+            ptr=ptr->next;
+            if(head->next){
+                pq.push(head->next);
             }
         }
-        ListNode*dummy=new ListNode(0);
-        ListNode* ptr=dummy;
-        while(!pq.empty()){
-            ptr->next=new ListNode(pq.top());
-            pq.pop();
-            ptr=ptr->next;
-        }
         return dummy->next;
-        
-        
     }
 };
