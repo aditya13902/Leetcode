1316343ca376a132f8f117d5d54bf29fa61352b3
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-13 01:40:14 +0530

    Time: 232 ms (22.34%), Space: 49.7 MB (9.70%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int>dist(n,1e9);
-        dist[src]=0;
+        vector<int>dis(n,1e8);
+        dis[src]=0;
         for(int i=0;i<=k;i++){
-            vector<int>temp=dist;
-            for(auto edge:flights){
-                if(edge[2]+dist[edge[0]]<temp[edge[1]]){
-                    temp[edge[1]]=edge[2]+dist[edge[0]];
+            vector<int>tmp=dis;
+            for(auto vec:flights){
+                if(tmp[vec[1]]>dis[vec[0]]+vec[2]){
+                    tmp[vec[1]]=dis[vec[0]]+vec[2];
                 }
             }
-            dist=temp;
+            dis=tmp;
         }
-        return dist[dst]==1e9 ?-1:dist[dst];
+        return dis[dst]==1e8?-1:dis[dst];
     }
 };
-// class Solution {
-// public:
-//     int findCheapestPrice(int n, vector<vector<int>>& edges, int src, int dst, int k) {
-//         vector<int> dist(n, INT_MAX);
-//         dist[src]=0;
-//         for(int i=0; i<=k; i++) {
-//             vector<int> temp(n);
-//             copy(dist.begin(), dist.end(), temp.begin());
-//             for(auto e : edges) {
-//                 if(dist[e[0]]==INT_MAX)
-//                     continue;
-//                 if(temp[e[1]]>dist[e[0]]+e[2])
-//                     temp[e[1]]=dist[e[0]]+e[2];
-//             }
-//             copy(temp.begin(), temp.end(), dist.begin());
-//         }
-//         if(dist[dst]==INT_MAX)
-//             return -1;
-//         return dist[dst];
-//     }
-// };
