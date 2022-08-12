59a48285c404d3e00eb38053f7e83522f18daf80
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-12 16:12:12 +0530

    Time: 19 ms (84.93%), Space: 16.7 MB (53.73%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    unordered_map<int,int>mp;
-    int helper(vector<int>&nums,int i){
-        if(i>=nums.size()-1) return 0;
-        if(mp.find(i)!=mp.end()) return mp[i];
-        int ans=1e8;
-        for(int j=i+1;j<=i+nums[i];j++){
-            ans=min(ans,1+helper(nums,j));
-        }
-        return mp[i]=ans;
-    }
     int jump(vector<int>& nums) {
-        return helper(nums,0)!=1e8?helper(nums,0):-1;
+        int prev=0;
+        int curr=0;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            if(prev<i){
+                if(curr>=i){
+                    prev=curr;
+                    cnt++;
+                }
+                else{
+                     return -1;
+                }
+            }
+            curr=max(curr,i+nums[i]);
+        }
+        return cnt;
     }
 };
