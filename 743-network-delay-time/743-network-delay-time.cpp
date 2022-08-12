577e74615bab80d408b30f9700085aa633036890
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-13 01:57:03 +0530

    Time: 248 ms (29.46%), Space: 43.8 MB (22.82%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int>dist(n+1,INT_MAX);
-        dist[k]=0;
-        for(int i=0;i<n-1;i++){
-            for(auto vec:times){
-                if(dist[vec[0]]!=INT_MAX && dist[vec[0]]+vec[2]<dist[vec[1]]){
-                    dist[vec[1]]=dist[vec[0]]+vec[2];
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(auto vec:times){
+            adj[vec[0]].push_back({vec[1],vec[2]});
+        }
+        vector<int>dis(n+1,1e8);
+        vector<int>vis(n+1,0);
+        set<pair<int,int>>st;
+        dis[k]=0;
+        st.insert({0,k});
+        while(st.size()){
+            auto curr=*(st.begin());
+            st.erase(st.begin());
+            int v=curr.second;
+            int d=curr.first;
+            if(vis[v]) continue;
+            vis[v]=1;
+            for(auto ele:adj[v]){
+                int ele_v=ele.first;
+                int ele_d=ele.second;
+                if(dis[ele_v]>dis[v]+ele_d){
+                    dis[ele_v]=dis[v]+ele_d;
+                    st.insert({dis[ele_v],ele_v});
                 }
             }
         }
         int ans=0;
         for(int i=1;i<=n;i++){
-            if(dist[i]==INT_MAX){
-                return -1;
-            }
-            ans=max(dist[i],ans);
+            if(dis[i]==1e8) return -1;
+            ans=max(ans,dis[i]);
         }
         return ans;
     }
