8e1badfdd64597a55ee8275ae2dd7ca00c585a12
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-13 09:05:43 +0530

    Time: 186 ms (9.02%), Space: 18.8 MB (9.82%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.cpp
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -1,54 +1,27 @@
 class Solution {
 public:
-    int n;    
-    bool dfs(vector<vector<int>> &grid, int i, int j, vector<vector<bool>> &visited, int maxtime){
-        // if value exceed the max value (m) then that means no possible solution from the current path
-        if(grid[i][j]>maxtime)
-            return false;
-        
-        //dfs
-        if(i==n-1 and j==n-1){
-            return true;
-        }
-        
-        visited[i][j] = true;
-        
-        int row[4] = {0,-1,0,1};
-        int col[4] = {-1,0,1,0};
-    
-        for(int k=0;k<4;k++){
-            if(i+row[k]>=0 and i+row[k]<n and j+col[k]>=0 and j+col[k]<n and !visited[i+row[k]][j+col[k]]){
-                if(dfs(grid, i+row[k], j+col[k], visited, maxtime))
-                    return true;
-            }
-        }
-        return false;
-    }
     int swimInWater(vector<vector<int>>& grid) {
-        // we need to minimize the maximum value for all the paths from top left to bottom right in grid
-        int time=INT_MAX;
-        n=grid.size();
-        
-        int l=0, h=n*n-1;
-        while(l<=h){
-            int m = l+(h-l)/2;
-            vector<vector<bool>> visited(n, vector<bool>(n,false)); // every time we refill the vector
+        int n=grid.size();
+        set<pair<int,int>>vis;
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        pq.push({grid[0][0],0,0});
+        while(pq.size()){
+            auto curr=pq.top();
+            pq.pop();
+            int mx=curr[0];
+            int x=curr[1];
+            int y=curr[2];
+            if(vis.find({x,y})!=vis.end()) continue;
+            vis.insert({x,y});
+            // mx=max(mx,grid[i][j]);
+            if(x==n-1 && y==n-1) return max(mx,grid[x][y]);
             
-            // we one by one take upper limit to be m (maximum height allowed)
-            if(dfs(grid, 0, 0, visited, m))
-            { 
-                // check whether there is a path to bottom right such that the max value is m
-                // if present then we update our answer and try for some lesser values
-                h=m-1;
-                time = m;
-            }
-            else
-            {
-                // if not then on every value less it will fail so we try for bigger values
-                l=m+1;
-            }
+            if(x-1>=0) pq.push({max(mx,grid[x-1][y]),x-1,y});
+            if(y-1>=0) pq.push({max(mx,grid[x][y-1]),x,y-1});
+            if(x+1<n) pq.push({max(mx,grid[x+1][y]),x+1,y});
+            if(y+1<n) pq.push({max(mx,grid[x][y+1]),x,y+1});
+            
         }
-        
-        return time;
+        return -1;
     }
 };
