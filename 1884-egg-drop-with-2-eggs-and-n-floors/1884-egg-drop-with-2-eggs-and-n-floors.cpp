d3e9c3f786d7d662ac053c4388c2dab7c9f9dae5
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-14 19:48:43 +0530

    Time: 4 ms (75.19%), Space: 6 MB (70.18%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int helper(int n,int k){
-        if(n==0)return 0;
-        if(k==0) return n;
-        if(dp[n][k]!=-1) return dp[n][k];
-        int ans=n;
-        for(int i=1;i<=n;i++){
-            ans=min(ans,1+max(helper(i-1,k-1),helper(n-i,k)));
-        }
-        return dp[n][k]=ans;
-    }
     int twoEggDrop(int n) {
-        dp.resize(n+1,vector<int>(2,-1));
-        return helper(n,1);
+        int tmp=1;
+        int cnt=0;
+        while(n>0){
+            n-=tmp;
+            cnt++;
+            tmp++;
+        }
+        return cnt;
     }
 };
