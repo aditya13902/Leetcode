75fab69a648e0f96d356d2645da38dbd9bb0ca2b
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-14 21:45:38 +0530

    Time: 357 ms (44.19%), Space: 84.9 MB (12.94%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -3,19 +3,18 @@ public:
     bool canPartition(vector<int>& nums) {
         int sum=0;
         for(auto ele:nums) sum+=ele;
-        if(sum%2 || nums.size()<2) return false;
-        int n=nums.size();
-        int curr=sum/2;
-        vector<vector<int>>dp(n+1,vector<int>(curr+1,0));
-        dp[0][0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=curr;j++){
-                dp[i][j]=dp[i-1][j];
-                if(j-nums[i-1]>=0){
-                    dp[i][j]|=dp[i-1][j-nums[i-1]];
-                }
+        if(sum%2) return false;
+        int tar=sum/2;
+        vector<vector<int>>dp(nums.size(),vector<int>(tar+1,0));
+        for(int i=0;i<nums.size();i++){
+            dp[i][0]=1;
+        }
+        if(tar>=nums[0]) dp[0][nums[0]]=1;
+        for(int i=1;i<nums.size();i++){
+            for(int j=1;j<=tar;j++){
+                dp[i][j]=dp[i-1][j] || ((j>=nums[i])? dp[i-1][j-nums[i]]: 0);
             }
         }
-        return dp[n][curr];
+        return dp[nums.size()-1][tar];
     }
 };
