3bc327781566ca32e46221f0e5772573efd48008
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-15 17:59:50 +0530

    Time: 12 ms (64.91%), Space: 7.3 MB (74.99%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         vector<int>mp1(26,0);
+        vector<int>mp2(26,0);
         for(auto ch:s1) mp1[ch-'a']++;
         int i=0;
-        vector<int>mp2(26,0);
         for(int j=0;j<s2.size();j++){
             mp2[s2[j]-'a']++;
             if(j-i>=s1.size()){
