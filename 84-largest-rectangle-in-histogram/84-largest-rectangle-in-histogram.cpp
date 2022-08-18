3e1f6a0fcfe4e430b6fea7a12244ca7157d97311
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-18 18:00:41 +0530

    Time: 277 ms (56.66%), Space: 81.5 MB (46.00%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int>lft(n,-1);
-        vector<int>rgt(n,n);
         stack<int>st;
-        for(int i=0;i<n;i++){
+        int n=heights.size();
+        vector<int>left(n,-1);
+        vector<int>right(n,n);
+        for(int i=0;i<heights.size();i++){
             while(st.size() && heights[st.top()]>=heights[i]){
                 st.pop();
             }
             if(st.size()){
-                lft[i]=st.top();
+                left[i]=st.top();
             }
             st.push(i);
-            
         }
         while(st.size()){
             st.pop();
         }
-        for(int i=n-1;i>=0;i--){
+        for(int i=heights.size()-1;i>=0;i--){
             while(st.size() && heights[st.top()]>=heights[i]){
                 st.pop();
             }
             if(st.size()){
-                rgt[i]=st.top();
+                right[i]=st.top();
             }
             st.push(i);
         }
         int ans=0;
-        for(int i=0;i<n;i++){
-            ans=max(ans,(rgt[i]-lft[i]-1)*heights[i]);
+        for(int i=0;i<heights.size();i++){
+            ans=max(ans,(right[i]-left[i]-1)*heights[i]);
         }
         return ans;
     }
