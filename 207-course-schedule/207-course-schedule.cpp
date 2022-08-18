a080003bbfdfc6cc98b41de61c54d42e38ef095a
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-18 19:57:36 +0530

    Time: 36 ms (49.73%), Space: 16 MB (9.67%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
+    vector<vector<int>>adj;
     vector<int>vis;
-    bool dfs(vector<vector<int>>&adj,int v,unordered_set<int>&st){
+    bool dfs(int v,unordered_set<int>&st){
         if(st.find(v)!=st.end()) return false;
         if(vis[v]) return true;
-        st.insert(v);
         vis[v]=1;
+        st.insert(v);
         for(auto ele:adj[v]){
-            if(!dfs(adj,ele,st)) return false;
+            if(!dfs(ele,st)) return false;
         }
         st.erase(v);
         return true;
     }
-    bool canFinish(int n, vector<vector<int>>& prereq) {
-        vis.resize(n,0);
-        vector<vector<int>>adj(n);
-        for(auto vec:prereq){
+    bool canFinish(int num, vector<vector<int>>& pre) {
+        adj.resize(num);
+        vis.resize(num,0);
+        for(auto vec:pre){
             adj[vec[0]].push_back(vec[1]);
         }
-        for(int i=0;i<n;i++){
-            if(vis[i]) continue;
-            unordered_set<int>st;
-            int flag=dfs(adj,i,st);
-            if(!flag) return false;
+        for(int i=0;i<num;i++){
+            if(!vis[i]){
+                unordered_set<int>st;
+                if(!dfs(i,st)) return false;
+            }
         }
         return true;
     }
