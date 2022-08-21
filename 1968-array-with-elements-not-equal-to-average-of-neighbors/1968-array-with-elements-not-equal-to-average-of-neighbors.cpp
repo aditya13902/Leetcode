b9d94ae80495240f504dd70071ff1db7ae2d05a7
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-21 18:52:06 +0530

    Time: 395 ms (52.16%), Space: 123.2 MB (74.61%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -2,9 +2,15 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i+=2){
-            swap(nums[i],nums[i-1]);
+        vector<int>arr(nums.size());
+        int l=0;
+        for(int i=0;l<nums.size();l+=2){
+            arr[l]=nums[i++];
         }
-        return nums;
+        int r=1;
+        for(int i=nums.size()-1;r<nums.size();r+=2){
+            arr[r]=nums[i--];
+        }
+        return arr;
     }
 };
