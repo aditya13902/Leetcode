78c391c4ebfad541067724a1fb697577a78bcca4
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-22 09:26:46 +0530

    Time: 13 ms (13.00%), Space: 9.7 MB (72.14%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         auto it=lower_bound(nums.begin(),nums.end(),target);// gives the iterator whose smallest value which is >=target
-        if(it!=nums.end() && *it==target){
-            return it-nums.begin();
-        }
+        // if(it!=nums.end() && *it==target){
+        //     return it-nums.begin();
+        // }
         return it-nums.begin();
     }
 };
