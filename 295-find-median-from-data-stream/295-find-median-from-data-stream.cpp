c5e5e5d75d7fe3c446ffce9ceaa3973f0f07ab83
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-08-30 15:15:25 +0530

    Time: 451 ms (69.11%), Space: 116.9 MB (98.27%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,42 +1,36 @@
 class MedianFinder {
 public:
-    priority_queue<double>maxheap;
-    priority_queue<double,vector<double>,greater<double>>minheap;
+    priority_queue<int>maxh;
+    priority_queue<int,vector<int>,greater<int>>minh;
     MedianFinder() {
         
     }
-    
+
     void addNum(int num) {
-        if(maxheap.size()==0 || maxheap.top()>=num){
-            maxheap.push(num);
-            if(maxheap.size()>minheap.size()+1){
-                minheap.push(maxheap.top());
-                maxheap.pop();
-            }    
-        }
-        else{
-            minheap.push(num);
-            if(minheap.size()>maxheap.size()){
-                // cout<<minheap.top()<<endl;
-                maxheap.push(minheap.top());
-                minheap.pop();
+        if(maxh.size()==0 || maxh.top()>num){
+            maxh.push(num);
+            if(maxh.size()>minh.size()+1){
+                minh.push(maxh.top());
+                maxh.pop();
             }
         }
-        // if(maxheap.size()){
-        //     cout<<"1"<<" "<<maxheap.top()<<endl;
-        // }
-        // if(minheap.size()){
-        //     cout<<"2"<<" "<<minheap.top()<<endl;
-        // }
-        
+        else{
+            minh.push(num);
+            if(minh.size()>maxh.size()){
+                maxh.push(minh.top());
+                minh.pop();
+            }
+        }
         
     }
     
     double findMedian() {
-        if(maxheap.size()==minheap.size()){
-            return (minheap.top()+maxheap.top())/2.0;
+        if(minh.size()==maxh.size()){
+            return ((double)(maxh.top()+minh.top()))/2.0;
         }
-        return maxheap.top();
+        else{
+             return maxh.top();
+        }
     }
 };
 
