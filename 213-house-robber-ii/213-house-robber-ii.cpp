980e8109444b780a7c438b3ef1987bc5f06842c0
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-01 11:40:06 +0530

    Time: 3 ms (58.57%), Space: 8 MB (20.30%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -2,11 +2,10 @@ class Solution {
 private:
     int helper(vector<int>&arr){
         if(arr.size()==0) return 0;
+        if(arr.size()==1) return arr[0];
         vector<int>dp(arr.size());
         dp[0]=arr[0];
-        if(arr.size()>1){
-            dp[1]=max(arr[1],arr[0]);
-        }
+        dp[1]=max(arr[1],arr[0]);
         for(int i=2;i<arr.size();i++){
             dp[i]=max(dp[i-1],dp[i-2]+arr[i]);
         }
