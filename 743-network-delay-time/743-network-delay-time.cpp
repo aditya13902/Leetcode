42417426094ab1f81e92ff2c2e2935688946e859
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-02 08:48:18 +0530

    Time: 136 ms (89.15%), Space: 44 MB (21.17%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -6,31 +6,26 @@ public:
             adj[vec[0]].push_back({vec[1],vec[2]});
         }
         vector<int>dis(n+1,1e8);
-        vector<int>vis(n+1,0);
-        set<pair<int,int>>st;
         dis[k]=0;
+        set<pair<int,int>>st;
         st.insert({0,k});
+        vector<int>vis(n+1,0);
         while(st.size()){
             auto curr=*(st.begin());
             st.erase(st.begin());
-            int v=curr.second;
-            int d=curr.first;
-            if(vis[v]) continue;
-            vis[v]=1;
-            for(auto ele:adj[v]){
-                int ele_v=ele.first;
-                int ele_d=ele.second;
-                if(dis[ele_v]>dis[v]+ele_d){
-                    dis[ele_v]=dis[v]+ele_d;
-                    st.insert({dis[ele_v],ele_v});
+            if(vis[curr.second]) continue;
+            vis[curr.second]=1;
+            for(auto ele:adj[curr.second]){
+                if(dis[ele.first]>dis[curr.second]+ele.second){
+                    dis[ele.first]=dis[curr.second]+ele.second;
+                    st.insert({dis[ele.first],ele.first});
                 }
             }
         }
-        int ans=0;
+        int mx=INT_MIN;
         for(int i=1;i<=n;i++){
-            if(dis[i]==1e8) return -1;
-            ans=max(ans,dis[i]);
+            mx=max(dis[i],mx);
         }
-        return ans;
+        return mx==1e8?-1:mx;
     }
 };
