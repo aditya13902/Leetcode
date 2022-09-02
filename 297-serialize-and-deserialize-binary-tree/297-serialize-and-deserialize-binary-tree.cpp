460c003a4f1744b1aeda41b76e29a893804a65dd
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-02 09:27:35 +0530

    Time: 690 ms (5.03%), Space: 715.1 MB (5.03%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -11,46 +11,41 @@ class Codec {
 public:
 
     // Encodes a tree to a single string.
-    void buildstr(TreeNode* root,string &res){
-        if(!root){
-            res+="null,";
+    void dfs(TreeNode* root,string &res){
+        if(!root) {
+            res+='N';
+            res+=',';
             return;
         }
-        res+=to_string(root->val)+",";
-        buildstr(root->left,res);
-        buildstr(root->right,res);
+        res+=(to_string(root->val));
+        res+=',';
+        dfs(root->left,res);
+        dfs(root->right,res);
     }
     string serialize(TreeNode* root) {
         string res="";
-        buildstr(root,res);
+        dfs(root,res);
         return res;
     }
 
     // Decodes your encoded data to tree.
-    TreeNode* buildtree(queue<string>&q){
-        auto top=q.front();
-        q.pop();
-        if(top=="null"){
+    TreeNode* helper(string data,int& idx){
+        string s="";
+        for(;data[idx]!=',';idx++){
+            s+=data[idx];
+        }
+        idx++;
+        if(s=="N"){
             return NULL;
         }
-        TreeNode* root=new TreeNode(stoi(top));
-        root->left=buildtree(q);
-        root->right=buildtree(q);
+        TreeNode* root=new TreeNode(stoi(s));
+        root->left=helper(data,idx);
+        root->right=helper(data,idx);
         return root;
     }
     TreeNode* deserialize(string data) {
-        queue<string>q;
-        string s="";
-        for(auto ch:data){
-            if(ch==','){
-                q.push(s);
-                s="";
-            }
-            else{
-                s+=ch;
-            }
-        }
-        return buildtree(q);
+        int i=0;
+        return helper(data,i);
     }
 };
 
