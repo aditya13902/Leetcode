f1577751f242e2f70ad8498cf97def2bad29df56
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-03 12:38:46 +0530

    Time: 12 ms (58.45%), Space: 9 MB (54.84%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,35 +1,32 @@
 class Solution {
+private:
+    vector<int>par;
 public:
-    vector<int>parent;
-    int getP(int v){
-        if(parent[v]==v){
-            return v;
-        }
-        return getP(parent[v]);
+    int find(int v){
+        if(par[v]==v) return v;
+        return par[v]=find(par[v]);
     }
-    bool merge(int v1,int v2){
-        v1=getP(v1);
-        v2=getP(v2);
-        if(v1!=v2){
-            parent[v1]=v2;
+    bool Union(int u,int v){
+        u=find(u);
+        v=find(v);
+        if(u!=v){
+            par[v]=u;
             return true;
         }
         return false;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        parent.resize(edges.size()+1);
-        for(int i=1;i<=edges.size();i++){
-            parent[i]=i;
-        }
-        vector<int>ans;
-        for(auto vec:edges){
-            int u=vec[0];
-            int v=vec[1];
-            if(!merge(u,v)){
-                ans=vec;
-                break;
+        int n=edges.size();
+        par.resize(n+1);
+        for(int i=1;i<=n;i++) par[i]=i;
+        
+        for(auto edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            if(!Union(u,v)){
+                return {u,v};
             }
         }
-        return ans;
+        return {};
     }
 };
