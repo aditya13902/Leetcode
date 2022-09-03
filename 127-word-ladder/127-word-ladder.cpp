26482b5c688ddb4faf7852e94085968701f24f94
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-03 13:00:30 +0530

    Time: 83 ms (96.52%), Space: 13.9 MB (76.77%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string>st;
+        for(auto str:wordList){
+            st.insert(str);
+        }
         queue<string>q;
         q.push(beginWord);
-        // insert into set (from word list)
-        unordered_set<string>st;
-        for(auto str:wordList) st.insert(str);
-        
-        if(st.find(endWord)==st.end()) return 0;
-        
-        int cnt=0;
-        while(q.size()){ // simple bfs
-            int len=q.size();
-            while(len--){
+        int cnt=1;
+        while(q.size()){
+            int l=q.size();
+            while(l--){
                 auto curr=q.front();
                 q.pop();
-                if(curr==endWord) return cnt+1;
+                if(curr==endWord) return cnt;
                 for(int i=0;i<curr.size();i++){
-                    string tmp=curr;
                     for(char ch='a';ch<='z';ch++){
-                        tmp[i]=ch;
-                        if(tmp==curr) continue;
-                        
-                        if(st.find(tmp)!=st.end()){
-                            st.erase(tmp);
-                            q.push(tmp);
+                        char pos=curr[i];
+                        curr[i]=ch;
+                        if(st.find(curr)!=st.end()){
+                            st.erase(curr);
+                            q.push(curr);
                         }
+                        curr[i]=pos;
                     }
                 }
             }
