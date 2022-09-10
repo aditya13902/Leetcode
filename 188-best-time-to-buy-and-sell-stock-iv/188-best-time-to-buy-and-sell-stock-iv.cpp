6959f078732a08774af8513110c5bec78dcdf92d
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-10 13:21:08 +0530

    Time: 56 ms (15.86%), Space: 21.3 MB (6.51%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    vector<vector<vector<int>>>dp;
-    int func(vector<int>&prices,int idx,int k,int f){
+    
+    int func(vector<int>&prices,int idx,int k,int f,vector<vector<vector<int>>>&dp){
         if(k==0 || idx==prices.size()) return 0;
         if(dp[idx][k][f]!=-1){
             return dp[idx][k][f];
         }
         if(f){
-            return dp[idx][k][f]=max(func(prices,idx+1,k,f),-prices[idx]+func(prices,idx+1,k,!f));
+            return dp[idx][k][f]=max(func(prices,idx+1,k,f,dp),-prices[idx]+func(prices,idx+1,k,!f,dp));
         }
         else{
-            return dp[idx][k][f]=max(func(prices,idx+1,k,f),prices[idx]+func(prices,idx+1,k-1,!f));
+            return dp[idx][k][f]=max(func(prices,idx+1,k,f,dp),prices[idx]+func(prices,idx+1,k-1,!f,dp));
         }
     }
     int maxProfit(int k, vector<int>& prices) {
-        dp.resize(prices.size(),vector<vector<int>>(k+1,vector<int>(2,-1)));
-        return func(prices,0,k,1);
+        vector<vector<vector<int>>>dp(prices.size(),vector<vector<int>>(k+1,vector<int>(2,-1)));
+        return func(prices,0,k,1,dp);
     }
 };
