6a60d75d006158bc38d8d928aa5e8396a76704cb
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-12 09:17:02 +0530

    Time: 8 ms (72.03%), Space: 10.7 MB (27.39%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -9,23 +9,16 @@ public:
         int ans=0;
         while(i<=j){
             if(power>=tokens[i]){
-                // cout<<i<<endl;
-                
                 score++;
                 power-=tokens[i];
                 i++;
-                
-                // cout<<power<<endl;
             }
             else if(score>0){
-                // cout<<j<<endl;
                 power+=tokens[j];
                 j--;
                 score--;
-                // cout<<power<<endl;
             }
             else{
-                // cout<<"yes"<<endl;
                 break;
             }
             ans=max(ans,score);
