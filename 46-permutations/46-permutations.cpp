4a3fe5e6a291a0f8cf643b4a25d066402d219bef
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-12 22:17:49 +0530

    Time: 7 ms (40.92%), Space: 7.5 MB (98.66%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void helper(vector<int>&arr,int idx){
+    void helper(vector<int>&arr,int idx,vector<vector<int>>&ans){
         if(idx==arr.size()){
+            // base condition
             ans.push_back(arr);
             return;
         }
         for(int i=idx;i<arr.size();i++){
-            swap(arr[idx],arr[i]);
-            helper(arr,idx+1);
-            swap(arr[idx],arr[i]);
+            swap(arr[i],arr[idx]);
+            helper(arr,idx+1,ans);
+            swap(arr[i],arr[idx]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        helper(nums,0);
+        vector<vector<int>> ans;
+        helper(nums,0,ans);
         return ans;
     }
 };
