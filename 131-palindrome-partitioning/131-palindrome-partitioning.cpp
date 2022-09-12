a5051b5a2603e60ce3562f787ecf87cae0feff17
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-12 22:44:42 +0530

    Time: 246 ms (40.27%), Space: 75.7 MB (37.98%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    vector<vector<string>>ans;
-    bool pali(string str){
-        int i=0;
-        int j=str.size()-1;
-        while(i<j){
-            if(str[i]!=str[j]){
-                return false;
-            }
+    vector<vector<string>> ans;
+    bool flag(string& str,int i,int j){
+        while(i<=j && str[i]==str[j]){
             i++;
             j--;
         }
-        return true;
+        return i>j;
     }
-    void helper(string s,int idx,string str,vector<string>&curr){
+    void helper(string& s,int idx,vector<string>&vec){
         if(idx==s.size()){
-            ans.push_back(curr);
+            ans.push_back(vec);
             return;
         }
         for(int i=idx;i<s.size();i++){
-            str+=s[i];
-            if(pali(str)){
-                curr.push_back(str);
-                helper(s,i+1,"",curr);
-                curr.pop_back();
+            if(flag(s,idx,i)){
+                vec.push_back(s.substr(idx,i-idx+1));
+                helper(s,i+1,vec);
+                vec.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<string>curr;
-        helper(s,0,"",curr);
+        vector<string>vec;
+        helper(s,0,vec);
         return ans;
     }
 };
