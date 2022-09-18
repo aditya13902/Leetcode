c7594669b68d3305c7d41deae6c6e6c54fdad47b
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-18 20:55:52 +0530

    Time: 51 ms (5.15%), Space: 20.9 MB (7.33%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
+    int trap(vector<int>& arr) {
+        int n=arr.size();
         vector<int>left(n);
         vector<int>right(n);
-        
         stack<int>st;
         for(int i=0;i<n;i++){
-            while(st.size() && st.top()<=height[i]){
+            while(st.size() && st.top()<=arr[i]){
                 st.pop();
             }
             if(st.size()){
                 left[i]=st.top();
             }
             else{
-                left[i]=height[i];
-                st.push(height[i]);
+                left[i]=arr[i];
+                st.push(arr[i]);
             }
         }
-        while(st.size()) st.pop();
-        
+        while(st.size()){
+            st.pop();
+        }
         for(int i=n-1;i>=0;i--){
-            while(st.size() && st.top()<=height[i]){
+            while(st.size() && st.top()<=arr[i]){
                 st.pop();
             }
-            if(st.size()) {
+            if(st.size()){
                 right[i]=st.top();
             }
             else{
-                right[i]=height[i];
-                st.push(height[i]);
+                right[i]=arr[i];
+                st.push(arr[i]);
             }
         }
         int ans=0;
         for(int i=0;i<n;i++){
-            ans+=(min(left[i],right[i])-height[i]);
+            ans+=(min(left[i],right[i])-arr[i]);
         }
         return ans;
     }
