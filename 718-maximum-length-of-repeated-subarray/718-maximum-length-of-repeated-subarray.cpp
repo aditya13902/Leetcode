952b81c11b4ce582a989213cefa1328133c35d6f
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-20 19:53:38 +0530

    Time: 923 ms (9.89%), Space: 109.3 MB (14.62%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
+    vector<vector<int>>dp;
+    // int helper(vector<int>&nums1,vector<int>&nums2,int i,int j,int f){
+    //     if(i>=nums1.size() || j>=nums2.size()){
+    //         return 0;
+    //     }
+    //     if(dp[i][j][f]!=-1){
+    //         return dp[i][j][f];
+    //     }
+    //     if(f){
+    //         int ans=max(helper(nums1,nums2,i+1,j,f),helper(nums1,nums2,i,j+1,f));
+    //         if(nums1[i]==nums2[j]){
+    //             ans=max(ans,1+helper(nums1,nums2,i+1,j+1,!f));
+    //         }
+    //         return dp[i][j][f]=ans;
+    //     }
+    //     else{
+    //         if(nums1[i]==nums2[j]){
+    //             return dp[i][j][f]=1+helper(nums1,nums2,i+1,j+1,f);
+    //         }
+    //         else{
+    //             return dp[i][j][f]=0;
+    //         }
+    //     }
+    // }
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        // dp.resize(nums1.size(),vector<vector<int>>(nums2.size(),vector<int>(2,-1)));
+        // return helper(nums1,nums2,0,0,1);
+        dp.resize(nums1.size()+1,vector<int>(nums2.size()+1,0));
         int ans=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(nums1[i-1]==nums2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=0;
-                
-                ans=max(ans,dp[i][j]);
+        for(int i=1;i<=nums1.size();i++){
+            for(int j=1;j<=nums2.size();j++){
+                if(nums1[i-1]==nums2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=0;
+                }
+                ans=max(dp[i][j],ans);
             }
         }
         return ans;
