097cffbfa4d0a46835e8b271a4238d037cb5a288
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-09-22 15:03:50 +0530

    Time: 452 ms (6.70%), Space: 49.6 MB (10.46%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int>dis(n,1e9);
+        vector<int>dis(n,1e8);
         dis[src]=0;
-        for(int t=0;t<=k;t++){
+        for(int i=0;i<=k;i++){
             vector<int>tmp=dis;
             for(auto vec:flights){
-                if(dis[vec[1]]>tmp[vec[0]]+vec[2]){
-                    dis[vec[1]]=tmp[vec[0]]+vec[2];
+                if(tmp[vec[1]]>dis[vec[0]]+vec[2]){
+                    tmp[vec[1]]=dis[vec[0]]+vec[2];
                 }
             }
+            dis=tmp;
         }
-        return dis[dst]==1e9?-1:dis[dst];
+        return dis[dst]>=1e8?-1:dis[dst];
     }
 };
