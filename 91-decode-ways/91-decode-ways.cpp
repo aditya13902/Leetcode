3b662f41d9debf91b94b40113de4cb7953a67db0
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-01 18:05:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (93.04%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    unordered_map<int,int>mp;
-    int ways(string &s,int i){
+    vector<int>dp;
+    int helper(string &s,int i){
         if(i==s.size()){
             return 1;
         }
-        if(s[i]=='0'){
-            return 0;
+        if(s[i]=='0') return 0;
+        if(dp[i]!=-1) return dp[i];
+        int ans=helper(s,i+1);
+        if(i+1<s.size() && s.substr(i,2)<="26"){
+            ans+=helper(s,i+2);
         }
-        if(mp.find(i)!=mp.end()){
-            return mp[i];
-        }
-        int ans=ways(s,i+1);
-        if(i+2<=s.size() && s.substr(i,2)<="26"){
-            ans+=ways(s,i+2);
-        }
-        return mp[i]=ans;
+        return dp[i]=ans;
     }
     int numDecodings(string s) {
-        return ways(s,0);
+        dp.resize(s.size(),-1);
+        return helper(s,0);
     }
 };
