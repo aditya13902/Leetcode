3226484a9150e3da6cdea6c6a3b2f6c92a662e92
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-01 18:12:26 +0530

    Time: 8 ms (18.06%), Space: 6.2 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -14,7 +14,17 @@ public:
         return dp[i]=ans;
     }
     int numDecodings(string s) {
-        dp.resize(s.size(),-1);
-        return helper(s,0);
+        int n=s.size();
+        dp.resize(n+1,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]!='0'){
+                dp[i]+=dp[i+1];
+                if(i+2<=n && s.substr(i,2)<="26"){
+                    dp[i]+=dp[i+2];
+                }
+            }
+        }
+        return dp[0];
     }
 };
