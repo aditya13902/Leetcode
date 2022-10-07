3e52a3f731d2744453cc540a63c1716497b0bb50
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-07 18:44:50 +0530

    Time: 81 ms (8.98%), Space: 12.7 MB (62.69%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -11,16 +11,18 @@ public:
     //     return memo[i][j]=ans;
     // }
     int longestCommonSubsequence(string text1, string text2) {
-        dp.resize(text1.size()+1,vector<int>(text2.size()+1,0));
+        vector<int>prev(text2.size()+1,0);
         for(int i=1;i<=text1.size();i++){
+            vector<int>curr(text2.size()+1,0);
             for(int j=1;j<=text2.size();j++){
                 if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=max(dp[i][j],1+dp[i-1][j-1]);
+                    curr[j]=max(curr[j],1+prev[j-1]);
                 }
-                dp[i][j]=max({dp[i][j],dp[i-1][j],dp[i][j-1]});
+                curr[j]=max({curr[j],prev[j],curr[j-1]});
             }
+            prev=curr;
         }
-        return dp[text1.size()][text2.size()];
+        return prev[text2.size()];
         
     }
 };
