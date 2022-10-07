c99144826081ca75d847ea0e0548c0a48f7649b6
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-07 18:42:33 +0530

    Time: 78 ms (10.49%), Space: 12.9 MB (58.65%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>>memo;
-    int rec(string &s,string &t,int i,int j){
-        if(i==s.size() || j==t.size()) return 0;
-        if(memo[i][j]!=-1) return memo[i][j];
-        int ans=max(rec(s,t,i+1,j),rec(s,t,i,j+1));
-        if(s[i]==t[j]){
-            ans=max(ans,1+rec(s,t,i+1,j+1));
-        }
-        return memo[i][j]=ans;
-    }
+    vector<vector<int>>dp;
+    // int rec(string &s,string &t,int i,int j){
+    //     if(i==s.size() || j==t.size()) return 0;
+    //     if(memo[i][j]!=-1) return memo[i][j];
+    //     int ans=max(rec(s,t,i+1,j),rec(s,t,i,j+1));
+    //     if(s[i]==t[j]){
+    //         ans=max(ans,1+rec(s,t,i+1,j+1));
+    //     }
+    //     return memo[i][j]=ans;
+    // }
     int longestCommonSubsequence(string text1, string text2) {
-        memo.resize(text1.size(),vector<int>(text2.size(),-1));
-        return rec(text1,text2,0,0);
+        dp.resize(text1.size()+1,vector<int>(text2.size()+1,0));
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=max(dp[i][j],1+dp[i-1][j-1]);
+                }
+                dp[i][j]=max({dp[i][j],dp[i-1][j],dp[i][j-1]});
+            }
+        }
+        return dp[text1.size()][text2.size()];
+        
     }
 };
