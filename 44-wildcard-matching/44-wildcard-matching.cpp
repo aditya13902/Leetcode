b4df121fa0a66922560ebafac5eb68920aeaf290
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-08 01:42:58 +0530

    Time: 69 ms (72.29%), Space: 25.8 MB (50.09%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -3,25 +3,23 @@ public:
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1));
-        dp[0][0]=1;
-        // for(int i=0;i<=n;i++){
-        //     if()
-        // }
+        vector<int>prev(p.size()+1,0);
+        prev[0]=1;
         for(int j=1;j<=m;j++){
             if(p[j-1]!='*') break;
-            dp[0][j]=1;
-            // cout<<"y"<<endl;
+            prev[j]=1;
         }
         for(int i=1;i<=n;i++){
+            vector<int>curr(m+1,0);
             for(int j=1;j<=m;j++){
                 if(s[i-1]==p[j-1] || p[j-1]=='?'){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
                 }
                 else if(p[j-1]=='*'){
-                    dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                    curr[j]=prev[j] || curr[j-1];
                 }
             }
+            prev=curr;
         }
         // for(int i=0;i<=n;i++){
         //     for(int j=0;j<=m;j++){
@@ -29,6 +27,6 @@ public:
         //     }
         //     cout<<endl;
         // }
-        return dp[n][m];
+        return prev[m];
     }
 };
