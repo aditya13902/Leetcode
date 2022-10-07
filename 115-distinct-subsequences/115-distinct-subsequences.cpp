8f158d4f1d1bea1d69cd5b2a7c3715c944b40668
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-07 22:57:59 +0530

    Time: 168 ms (5.00%), Space: 29.9 MB (31.62%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,18 +1,21 @@
+const int mod=1e9+7;
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int helper(string &s,string &t,int i,int j){
-        if(j==t.size()) return 1;
-        if(i==s.size()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans=helper(s,t,i+1,j);
-        if(s[i]==t[j]){
-            ans+=helper(s,t,i+1,j+1);
-        }
-        return dp[i][j]=ans;
-    }
     int numDistinct(string s, string t) {
-        dp.resize(s.size(),vector<int>(t.size(),-1));
-        return helper(s,t,0,0);
+        int n=s.size();
+        int m=t.size();
+        vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                dp[i][j]=dp[i-1][j];
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=(dp[i][j]+dp[i-1][j-1])%mod;
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
