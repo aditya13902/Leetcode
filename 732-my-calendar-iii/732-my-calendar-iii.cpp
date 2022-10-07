4b046ee4a96f1921856b7d910afc779b838fa762
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-07 10:43:13 +0530

    Time: 121 ms (90.88%), Space: 26.4 MB (37.06%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -0,0 +1,25 @@
+class MyCalendarThree {
+public:
+    map<int,int>mp;
+    MyCalendarThree() {
+        
+    }
+    
+    int book(int start, int end) {
+        mp[start]++;
+        mp[end]--;
+        int ans=0;
+        int curr=0;
+        for(auto it:mp){
+            curr+=it.second;
+            ans=max(ans,curr);
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
