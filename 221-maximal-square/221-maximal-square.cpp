c15fb604c3bf435dae0710150315e193e94757a4
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-08 21:53:12 +0530

    Time: 198 ms (30.98%), Space: 21 MB (13.37%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int row=matrix.size();
-        int col=matrix[0].size();
-        vector<vector<int>>dp(row,vector<int>(col,0));
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
         int ans=0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
                 if(matrix[i][j]=='1'){
-                    // cout<<"yes"<<endl;
-                    ans=1;
                     dp[i][j]=1;
+                    ans=1;
                 }
             }
         }
-        for(int i=row-2;i>=0;i--){
-            for(int j=col-2;j>=0;j--){
-                if(dp[i][j]==1){
-                    // cout<<"yes"<<endl;
-                    dp[i][j]=1+min({dp[i+1][j],dp[i+1][j+1],dp[i][j+1]});
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                int curr=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                // int curr=0;
+                if(matrix[i][j]=='1'){
+                    dp[i][j]=1+curr;
+                }
+                else{
+                    dp[i][j]=0;
                 }
                 ans=max(ans,dp[i][j]);
             }
+            
         }
         return ans*ans;
-        
-        
     }
 };
