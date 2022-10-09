9dee120deec9b6226a9d5ad90400d6da410ad700
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-09 15:09:18 +0530

    Time: 75 ms (53.19%), Space: 36.8 MB (79.97%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,52 +1,73 @@
-class BSTIterator {
-    stack<TreeNode *> myStack;
-    bool reverse = true; 
-public:
-    BSTIterator(TreeNode *root, bool isReverse) {
-        reverse = isReverse; 
-        pushAll(root);
-    }
-
-    /** @return whether we have a next smallest number */
-    bool hasNext() {
-        return !myStack.empty();
-    }
-
-    /** @return the next smallest number */
-    int next() {
-        TreeNode *tmpNode = myStack.top();
-        myStack.pop();
-        if(!reverse) pushAll(tmpNode->right);
-        else pushAll(tmpNode->left);
-        return tmpNode->val;
-    }
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BstIterator{
 private:
-    void pushAll(TreeNode *node) {
-        for(;node != NULL; ) {
-             myStack.push(node);
-             if(reverse == true) {
-                 node = node->right; 
-             } else {
-                 node = node->left; 
-             }
+    stack<TreeNode*>st;
+    bool f;
+public:
+    BstIterator(TreeNode* root,int f){
+        this->f=f;
+        pushall(root);
+    }
+    void pushall(TreeNode*root){
+        if(f){
+            while(root){
+                st.push(root);
+                root=root->left;
+            }
         }
+        else{
+            while(root){
+                st.push(root);
+                root=root->right;
+            }
+        }
+    }
+    int get(){
+        auto node=st.top();
+        st.pop();
+        if(f){
+            if(node->right){
+                pushall(node->right);
+            }
+        }
+        else{
+            if(node->left){
+                pushall(node->left);
+            }
+        }
+        return node->val;
     }
 };
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
-        if(!root) return false; 
-        BSTIterator l(root, false); 
-        BSTIterator r(root, true); 
-        
-        int i = l.next(); 
-        int j = r.next(); 
-        while(i<j) {
-            if(i + j == k) return true; 
-            else if(i + j < k) i = l.next(); 
-            else j = r.next(); 
+        if(!root) return false;
+        BstIterator* l=new BstIterator(root,1);
+        BstIterator* r=new BstIterator(root,0);
+        int i=l->get();
+        int j=r->get();
+        while(i<j){
+            if(i+j==k){
+                return true;
+            }
+            else if(i+j<k){
+                i=l->get();
+            }
+            else{
+                j=r->get();
+            }
         }
-        return false; 
+        return false;
+        
     }
 };
