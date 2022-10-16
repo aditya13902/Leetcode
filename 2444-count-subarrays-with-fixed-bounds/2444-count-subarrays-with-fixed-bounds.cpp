b9b1f90b0b809500b58ca9caf2286b85c2f3fa89
Aditya Kumar <76881511+aditya13902@users.noreply.github.com>
2022-10-16 18:31:46 +0530

    Time: 135 ms (66.67%), Space: 70.4 MB (11.11%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long res = 0;
-        bool minFound = false, maxFound = false;
-        int start = 0, minStart = 0, maxStart = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int num = nums[i];
-            if (num < minK || num > maxK) {
-                minFound = false;
-                maxFound = false;
-                start = i+1;
+        bool fmn=0;
+        bool fmx=0;
+        int strt=0;
+        int min_idx=-1;
+        int max_idx=-1;
+        long long ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<minK || nums[i]>maxK){
+                fmn=0;
+                fmx=0;
+                min_idx=i;
+                max_idx=i;
+                strt=i+1;
             }
-            if (num == minK) {
-                minFound = true;
-                minStart = i;
+            if(nums[i]==minK){
+                fmn=1;
+                min_idx=i;
             }
-            if (num == maxK) {
-                maxFound = true;
-                maxStart = i;
+            if(nums[i]==maxK){
+                fmx=1;
+                max_idx=i;
             }
-            if (minFound && maxFound) {
-                res += (min(minStart, maxStart) - start + 1);
+            if(fmn && fmx){
+                ans+=(min(max_idx,min_idx)-strt+1);
             }
         }
-        return res;
+        return ans;
     }
 };
